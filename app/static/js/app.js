1c4d596bf346befa0b72a255e926b1901c71dfee
Michael Lynch <git@mtlynch.io>
2020-06-23 11:00:37 -0400

    Removing old onPowerButtonClick handler

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -185,49 +185,6 @@ function onDisplayHistoryChanged(evt) {
   }
 }
 
-function onPowerButtonClick() {
-  fetch("/shutdown", {
-    method: "POST",
-    headers: {
-      "X-CSRFToken": getCsrfToken(),
-    },
-    mode: "same-origin",
-    cache: "no-cache",
-    redirect: "error",
-  })
-    .then((response) => {
-      if (response.status !== 200) {
-        // See if the error response is JSON.
-        const contentType = response.headers.get("content-type");
-        if (contentType && contentType.indexOf("application/json") !== -1) {
-          return response.json().then((data) => {
-            return Promise.reject(new Error(data.error));
-          });
-        }
-        return Promise.reject(new Error(response.statusText));
-      }
-      return response.json();
-    })
-    .then((result) => {
-      if (result.error) {
-        return Promise.reject(new Error(result.error));
-      }
-      poweringDown = true;
-      displayPoweringDownUI();
-    })
-    .catch((error) => {
-      // Depending on timing, the server may not respond to the shutdown request
-      // because it's shutting down. If we get a NetworkError, assume the
-      // shutdown succeeded.
-      if (error.message.indexOf("NetworkError") >= 0) {
-        poweringDown = true;
-        displayPoweringDownUI();
-        return;
-      }
-      showError("Failed to Shut Down KVM Pi Device", error);
-    });
-}
-
 document.querySelector("body").addEventListener("keydown", onKeyDown);
 document
   .getElementById("display-history-checkbox")
