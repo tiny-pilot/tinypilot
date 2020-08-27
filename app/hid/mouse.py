1bff09fa4246a739cd6acbadf4f7592506422b05
Sander Kromwijk <s.kromwijk@gmail.com>
2020-08-26 22:55:50 -0400

    Off-by-one comments...

--- app/hid/mouse.py
+++ app/hid/mouse.py
@@ -12,13 +12,13 @@ def send_mouse_position(mouse_path, x, y, mouse_down, mouse_button):
     buf = [0] * 5
     if mouse_down:
         if mouse_button == 2:
-            # Right button (set bit 2)
+            # Right button (set bit 1)
             buf[0] = 0x02
         elif mouse_button == 1:
-            # Middle button (set bit 3)
+            # Middle button (set bit 2)
             buf[0] = 0x04
         else:
-            # Assume left button (set bit 1)
+            # Assume left button (set bit 0)
             buf[0] = 0x01
     else:
         buf[0] = 0
