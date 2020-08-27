a93018ea09aa84200e34d37ad5eb945771a45c11
Michael Lynch <mtlynch@users.noreply.github.com>
2020-08-27 14:48:23 -0400

    Add support for mouse input (#125)
    * Add support for mouse input
    
    Fixes #40
    
    * Delete redundant comment
    
    * Adding MOUSE_PATH to README
    
    * Adding more implementation pieces to support mouse movement
    
    * Adding more of mouse implementation
    
    * Reduce slashes
    
    * Update cursor and usb gadget init
    
    * Add more mouse implementation
    
    * Movement and clicking work, but need cleanup
    
    * Move mouse HID code
    
    * Delete debug code
    
    * Mouse tweaks (#162)
    
    * Mouse tweaks
    
    * Address formatter feedback
    
    * Off-by-one comments...
    
    * Fix bad merge
    
    * Switching to mouse buttons property
    
    * Disable context menu on remote screen
    
    * Add TODO
    
    * Delete unused global variable
    
    * Clean up mouse code
    
    * Fix mouse event send
    
    * Fix variable names
    
    * Add unit tests
    
    * Add more tests
    
    * Trim unneeded code
    
    Co-authored-by: Sander Kromwijk <s.kromwijk@gmail.com>

---
+++ app/tests/request_parsers/test_mouse_event.py
@@ -0,0 +1,96 @@
+import unittest
+
+from request_parsers import mouse_event
+
+
+class MouseEventTest(unittest.TestCase):
+
+    def test_parses_valid_mouse_event(self):
+        self.assertEqual(
+            mouse_event.MouseEvent(buttons=1, relative_x=0.5, relative_y=0.75),
+            mouse_event.parse_mouse_event({
+                'buttons': 1,
+                'relativeX': 0.5,
+                'relativeY': 0.75,
+            }))
+
+    def test_parses_valid_mouse_event_with_all_buttons_pressed(self):
+        self.assertEqual(
+            mouse_event.MouseEvent(buttons=31, relative_x=0.5, relative_y=0.75),
+            mouse_event.parse_mouse_event({
+                'buttons': 31,
+                'relativeX': 0.5,
+                'relativeY': 0.75,
+            }))
+
+    def test_rejects_negative_buttons_value(self):
+        with self.assertRaises(mouse_event.InvalidButtonState):
+            mouse_event.parse_mouse_event({
+                'buttons': -1,
+                'relativeX': 0.5,
+                'relativeY': 0.75,
+            })
+
+    def test_rejects_too_high_buttons_value(self):
+        with self.assertRaises(mouse_event.InvalidButtonState):
+            mouse_event.parse_mouse_event({
+                'buttons': 32,
+                'relativeX': 0.5,
+                'relativeY': 0.75,
+            })
+
+    def test_rejects_non_numeric_buttons_value(self):
+        with self.assertRaises(mouse_event.InvalidButtonState):
+            mouse_event.parse_mouse_event({
+                'buttons': 'a',
+                'relativeX': 0.5,
+                'relativeY': 0.75,
+            })
+
+    def test_rejects_negative_relative_x_value(self):
+        with self.assertRaises(mouse_event.InvalidRelativePosition):
+            mouse_event.parse_mouse_event({
+                'buttons': 0,
+                'relativeX': -0.001,
+                'relativeY': 0.75,
+            })
+
+    def test_rejects_negative_relative_y_value(self):
+        with self.assertRaises(mouse_event.InvalidRelativePosition):
+            mouse_event.parse_mouse_event({
+                'buttons': 0,
+                'relativeX': 0.5,
+                'relativeY': -0.001,
+            })
+
+    def test_rejects_too_high_relative_x_value(self):
+        with self.assertRaises(mouse_event.InvalidRelativePosition):
+            mouse_event.parse_mouse_event({
+                'buttons': 0,
+                'relativeX': 1.001,
+                'relativeY': 0.75,
+            })
+
+    def test_rejects_too_high_relative_y_value(self):
+        with self.assertRaises(mouse_event.InvalidRelativePosition):
+            mouse_event.parse_mouse_event({
+                'buttons': 0,
+                'relativeX': 0.5,
+                'relativeY': 1.001,
+            })
+
+    def test_rejects_non_float_relative_x_value(self):
+        with self.assertRaises(mouse_event.InvalidRelativePosition):
+            mouse_event.parse_mouse_event({
+                'buttons': 0,
+                'relativeX': 'a',
+                'relativeY': 0.75,
+            })
+
+    def test_rejects_non_float_relative_y_value(self):
+        with self.assertRaises(mouse_event.InvalidRelativePosition):
+            mouse_event.parse_mouse_event({
+                'buttons': 0,
+                'relativeX': 0.5,
+                'relativeY': 'b',
+            })
