d192c48d9341085f9f98afd1440def514d961828
Michael Lynch <git@mtlynch.io>
2020-09-17 15:54:20 -0400

    Refactor keycodes
    The existing ones are QWERTY-specifc, so moving them to a QWERTY-specific module.

--- app/js_to_hid.py
+++ app/js_to_hid.py
@@ -1,4 +1,6 @@
-from hid import keyboard
+from hid.keycodes import qwerty
+
+_LAYOUT = qwerty
 
 
 class Error(Exception):
@@ -10,133 +12,132 @@ class UnrecognizedKeyCodeError(Error):
 
 
 # JS keycodes source: https://github.com/wesbos/keycodes
-# HID keycodes source: https://gist.github.com/MightyPork/6da26e382a7ad91b5496ee55fdc73db2
 
 # TODO: For simplicity, we map all modifiers keys to the left key, but we could
 # support distinct keys for left and right if we check the location parameter
 # from the JavaScript message.
 _JS_TO_HID_KEYCODES = {
-    3: 0x48,  # Pause / Break
-    8: 0x2a,  # Backspace / Delete
-    9: 0x2b,  # Tab
-    12: 0x53,  # Clear
-    13: 0x28,  # Enter
-    16: keyboard.KEYCODE_LEFT_SHIFT,
-    17: keyboard.KEYCODE_LEFT_CTRL,
-    18: keyboard.KEYCODE_LEFT_ALT,
-    19: 0x48,  # Pause / Break
-    20: 0x39,  # Caps Lock
-    21: 0x90,  # Hangeul
-    25: 0x91,  # Hanja
-    27: 0x29,  # Escape
-    32: 0x2c,  # Spacebar
-    33: 0x4b,  # Page Up
-    34: 0x4e,  # Page Down
-    35: 0x4d,  # End
-    36: 0x4a,  # Home
-    37: 0x50,  # Left Arrow
-    38: 0x52,  # Up Arrow
-    39: 0x4f,  # Right Arrow
-    40: 0x51,  # Down Arrow
-    41: 0x77,  # Select
-    43: 0x74,  # Execute
-    44: 0x46,  # Print Screen
-    45: 0x49,  # Insert
-    46: 0x4c,  # Delete
-    47: 0x75,  # Help
-    48: 0x27,  # 0
-    49: 0x1e,  # 1
-    50: 0x1f,  # 2
-    51: 0x20,  # 3
-    52: 0x21,  # 4
-    53: 0x22,  # 5
-    54: 0x23,  # 6
-    55: 0x24,  # 7
-    56: 0x25,  # 8
-    57: 0x26,  # 9
-    59: 0x33,  # Semicolon
-    60: 0xc5,  # <
-    61: 0x2e,  # Equal sign
-    65: 0x04,  # a
-    66: 0x05,  # b
-    67: 0x06,  # c
-    68: 0x07,  # d
-    69: 0x08,  # e
-    70: 0x09,  # f
-    71: 0x0a,  # g
-    72: 0x0b,  # h
-    73: 0x0c,  # i
-    74: 0x0d,  # j
-    75: 0x0e,  # k
-    76: 0x0f,  # l
-    77: 0x10,  # m
-    78: 0x11,  # n
-    79: 0x12,  # o
-    80: 0x13,  # p
-    81: 0x14,  # q
-    82: 0x15,  # r
-    83: 0x16,  # s
-    84: 0x17,  # t
-    85: 0x18,  # u
-    86: 0x19,  # v
-    87: 0x1a,  # w
-    88: 0x1b,  # x
-    89: 0x1c,  # y
-    90: 0x1d,  # z
-    91: keyboard.KEYCODE_LEFT_META,
-    94: 0x5c,  # \ (UK)
-    96: 0x62,  # Numpad 0
-    97: 0x59,  # Numpad 1
-    98: 0x5a,  # Numpad 2
-    99: 0x5b,  # Numpad 3
-    100: 0x5c,  # Numpad 4
-    101: 0x5d,  # Numpad 5
-    102: 0x5e,  # Numpad 6
-    103: 0x5f,  # Numpad 7
-    104: 0x60,  # Numpad 8
-    105: 0x61,  # Numpad 9
-    112: 0x3a,  # F1
-    113: 0x3b,  # F2
-    114: 0x3c,  # F3
-    115: 0x3d,  # F4
-    116: 0x3e,  # F5
-    117: 0x3f,  # F6
-    118: 0x40,  # F7
-    119: 0x41,  # F8
-    120: 0x42,  # F9
-    121: 0x43,  # F10
-    122: 0x44,  # F11
-    123: 0x45,  # F12
-    124: 0x68,  # F13
-    125: 0x69,  # F14
-    126: 0x6a,  # F15
-    127: 0x6b,  # F16
-    128: 0x6c,  # F17
-    129: 0x6d,  # F18
-    130: 0x6e,  # F19
-    131: 0x6f,  # F20
-    132: 0x70,  # F21
-    133: 0x71,  # F22
-    134: 0x72,  # F23
-    144: 0x53,  # Num Lock
-    145: 0x47,  # Scroll Lock
-    161: 0x1e,  # !
-    163: 0x32,  # Hash
-    173: 0x2d,  # Minus
-    179: 0xe8,  # Media play/pause
-    168: 0xfa,  # Refresh
-    186: 0x33,  # Semicolon
-    187: 0x2e,  # Equal sign
-    188: 0x36,  # Comma
-    189: 0x2d,  # Minus sign
-    190: 0x37,  # Period
-    191: 0x38,  # Forward slash
-    192: 0x35,  # Accent grave
-    219: 0x2f,  # Left bracket ([, {)
-    220: 0x31,  # Back slash
-    221: 0x30,  # Right bracket (], })
-    222: 0x34,  # Single quote
-    223: 0x35,  # Accent grave (`)
+    3: _LAYOUT.KEYCODE_PAUSE_BREAK,
+    8: _LAYOUT.KEYCODE_BACKSPACE_DELETE,
+    9: _LAYOUT.KEYCODE_TAB,
+    12: _LAYOUT.KEYCODE_CLEAR,
+    13: _LAYOUT.KEYCODE_ENTER,
+    16: _LAYOUT.KEYCODE_LEFT_SHIFT,
+    17: _LAYOUT.KEYCODE_LEFT_CTRL,
+    18: _LAYOUT.KEYCODE_LEFT_ALT,
+    19: _LAYOUT.KEYCODE_PAUSE_BREAK,
+    20: _LAYOUT.KEYCODE_CAPS_LOCK,
+    21: _LAYOUT.KEYCODE_HANGEUL,
+    25: _LAYOUT.KEYCODE_HANJA,
+    27: _LAYOUT.KEYCODE_ESCAPE,
+    32: _LAYOUT.KEYCODE_SPACEBAR,
+    33: _LAYOUT.KEYCODE_PAGE_UP,
+    34: _LAYOUT.KEYCODE_PAGE_DOWN,
+    35: _LAYOUT.KEYCODE_END,
+    36: _LAYOUT.KEYCODE_HOME,
+    37: _LAYOUT.KEYCODE_LEFT_ARROW,
+    38: _LAYOUT.KEYCODE_UP_ARROW,
+    39: _LAYOUT.KEYCODE_RIGHT_ARROW,
+    40: _LAYOUT.KEYCODE_DOWN_ARROW,
+    41: _LAYOUT.KEYCODE_SELECT,
+    43: _LAYOUT.KEYCODE_EXECUTE,
+    44: _LAYOUT.KEYCODE_PRINT_SCREEN,
+    45: _LAYOUT.KEYCODE_INSERT,
+    46: _LAYOUT.KEYCODE_DELETE,
+    47: _LAYOUT.KEYCODE_HELP,
+    48: _LAYOUT.KEYCODE_NUMBER_0,
+    49: _LAYOUT.KEYCODE_NUMBER_1,
+    50: _LAYOUT.KEYCODE_NUMBER_2,
+    51: _LAYOUT.KEYCODE_NUMBER_3,
+    52: _LAYOUT.KEYCODE_NUMBER_4,
+    53: _LAYOUT.KEYCODE_NUMBER_5,
+    54: _LAYOUT.KEYCODE_NUMBER_6,
+    55: _LAYOUT.KEYCODE_NUMBER_7,
+    56: _LAYOUT.KEYCODE_NUMBER_8,
+    57: _LAYOUT.KEYCODE_NUMBER_9,
+    59: _LAYOUT.KEYCODE_SEMICOLON,
+    60: _LAYOUT.KEYCODE_LESS_THAN,
+    61: _LAYOUT.KEYCODE_EQUAL_SIGN,
+    65: _LAYOUT.KEYCODE_A,
+    66: _LAYOUT.KEYCODE_B,
+    67: _LAYOUT.KEYCODE_C,
+    68: _LAYOUT.KEYCODE_D,
+    69: _LAYOUT.KEYCODE_E,
+    70: _LAYOUT.KEYCODE_F,
+    71: _LAYOUT.KEYCODE_G,
+    72: _LAYOUT.KEYCODE_H,
+    73: _LAYOUT.KEYCODE_I,
+    74: _LAYOUT.KEYCODE_J,
+    75: _LAYOUT.KEYCODE_K,
+    76: _LAYOUT.KEYCODE_L,
+    77: _LAYOUT.KEYCODE_M,
+    78: _LAYOUT.KEYCODE_N,
+    79: _LAYOUT.KEYCODE_O,
+    80: _LAYOUT.KEYCODE_P,
+    81: _LAYOUT.KEYCODE_Q,
+    82: _LAYOUT.KEYCODE_R,
+    83: _LAYOUT.KEYCODE_S,
+    84: _LAYOUT.KEYCODE_T,
+    85: _LAYOUT.KEYCODE_U,
+    86: _LAYOUT.KEYCODE_V,
+    87: _LAYOUT.KEYCODE_W,
+    88: _LAYOUT.KEYCODE_X,
+    89: _LAYOUT.KEYCODE_Y,
+    90: _LAYOUT.KEYCODE_Z,
+    91: _LAYOUT.KEYCODE_LEFT_META,
+    94: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_4,  # / (UK)
+    96: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_0,
+    97: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_1,
+    98: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_2,
+    99: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_3,
+    100: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_4,
+    101: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_5,
+    102: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_6,
+    103: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_7,
+    104: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_8,
+    105: _LAYOUT.KEYCODE_KEYCODE_NUMPAD_9,
+    112: _LAYOUT.KEYCODE_F1,
+    113: _LAYOUT.KEYCODE_F2,
+    114: _LAYOUT.KEYCODE_F3,
+    115: _LAYOUT.KEYCODE_F4,
+    116: _LAYOUT.KEYCODE_F5,
+    117: _LAYOUT.KEYCODE_F6,
+    118: _LAYOUT.KEYCODE_F7,
+    119: _LAYOUT.KEYCODE_F8,
+    120: _LAYOUT.KEYCODE_F9,
+    121: _LAYOUT.KEYCODE_F10,
+    122: _LAYOUT.KEYCODE_F11,
+    123: _LAYOUT.KEYCODE_F12,
+    124: _LAYOUT.KEYCODE_F13,
+    125: _LAYOUT.KEYCODE_F14,
+    126: _LAYOUT.KEYCODE_F15,
+    127: _LAYOUT.KEYCODE_F16,
+    128: _LAYOUT.KEYCODE_F17,
+    129: _LAYOUT.KEYCODE_F18,
+    130: _LAYOUT.KEYCODE_F19,
+    131: _LAYOUT.KEYCODE_F20,
+    132: _LAYOUT.KEYCODE_F21,
+    133: _LAYOUT.KEYCODE_F22,
+    134: _LAYOUT.KEYCODE_F23,
+    144: _LAYOUT.KEYCODE_NUM_LOCK,
+    145: _LAYOUT.KEYCODE_SCROLL_LOCK,
+    161: _LAYOUT.KEYCODE_NUMBER_1,
+    163: _LAYOUT.KEYCODE_HASH,
+    173: _LAYOUT.KEYCODE_MINUS,
+    179: _LAYOUT.KEYCODE_MEDIA_PLAY_PAUSE,
+    168: _LAYOUT.KEYCODE_REFRESH,
+    186: _LAYOUT.KEYCODE_SEMICOLON,
+    187: _LAYOUT.KEYCODE_EQUAL_SIGN,
+    188: _LAYOUT.KEYCODE_COMMA,
+    189: _LAYOUT.KEYCODE_MINUS,
+    190: _LAYOUT.KEYCODE_PERIOD,
+    191: _LAYOUT.KEYCODE_FORWARD_SLASH,
+    192: _LAYOUT.KEYCODE_ACCENT_GRAVE,
+    219: _LAYOUT.KEYCODE_LEFT_BRACKET,
+    220: _LAYOUT.KEYCODE_BACKSLASH,
+    221: _LAYOUT.KEYCODE_RIGHT_BRACKET,
+    222: _LAYOUT.KEYCODE_SINGLE_QUOTE,
+    223: _LAYOUT.KEYCODE_ACCENT_GRAVE,
 }
 
 
