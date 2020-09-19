21fe46896c98bdf9f68c48e3e7cf657f4823d9a3
Michael Lynch <git@mtlynch.io>
2020-09-19 15:21:23 -0400

    Use regular open() function to write to the HID interface
    I initially switched to os.open because I thought we needed to O_NONBLOCK flag, but given that it's unnecessary, we can go back to the regular file open API.

--- app/hid/write.py
+++ app/hid/write.py
@@ -1,6 +1,3 @@
-import os
-
-
 class Error(Exception):
     pass
 
@@ -10,13 +7,9 @@ class WriteError(Error):
 
 
 def write_to_hid_interface(hid_path, buffer):
-    hid_fd = 0
     try:
-        hid_fd = os.open(hid_path, os.O_RDWR)
-        os.write(hid_fd, bytearray(buffer))
+        with open(hid_path, 'rb+') as hid_interface:
+            hid_interface.write(bytearray(buffer))
     except BlockingIOError:
         raise WriteError(
             'Failed to write to HID interface. Is USB cable connected?')
-    finally:
-        if hid_fd:
-            os.close(hid_fd)
