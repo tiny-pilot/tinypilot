69e5728417c863d771543b26d64519a2710e30b3
djclueless <djclueless@gmail.com>
2020-09-19 19:26:20 +0100

    Fix write exception 'Resource temporarily unavailable'

--- app/hid/write.py
+++ app/hid/write.py
@@ -12,7 +12,7 @@ class WriteError(Error):
 def write_to_hid_interface(hid_path, buffer):
     hid_fd = 0
     try:
-        hid_fd = os.open(hid_path, os.O_RDWR | os.O_NONBLOCK)
+        hid_fd = os.open(hid_path, os.O_RDWR)
         os.write(hid_fd, bytearray(buffer))
     except BlockingIOError:
         raise WriteError(
