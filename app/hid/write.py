f616dfd71e2a008163bbbd0540bdef1bd42212c0
Michael Lynch <git@mtlynch.io>
2020-09-19 14:36:39 -0400

    Add a comment explaining why we open the HID interface as O_NONBLOCK

--- app/hid/write.py
+++ app/hid/write.py
@@ -12,6 +12,9 @@ class WriteError(Error):
 def write_to_hid_interface(hid_path, buffer):
     hid_fd = 0
     try:
+        # Open the HID interface in non-blocking mode. Otherwise, if the write
+        # fails, for example due to a cable that's incompatible or broken, the
+        # write will hang indefinitely, locking up the process.
         hid_fd = os.open(hid_path, os.O_RDWR | os.O_NONBLOCK)
         os.write(hid_fd, bytearray(buffer))
     except BlockingIOError:
