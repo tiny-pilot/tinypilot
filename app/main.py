ae76a0af43dffd719eb152d4d09a95ff934131f4
Michael Lynch <mtlynch@users.noreply.github.com>
2020-10-13 15:26:43 -0400

    Add debug logging for HID writes (#291)

--- app/main.py
+++ app/main.py
@@ -16,17 +16,6 @@ from hid import write as hid_write
 from request_parsers import keystroke as keystroke_request
 from request_parsers import mouse_event as mouse_event_request
 
-root_logger = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = logging.Formatter(
-    '%(asctime)s %(name)-15s %(levelname)-4s %(message)s', '%Y-%m-%d %H:%M:%S')
-handler.setFormatter(formatter)
-root_logger.addHandler(flask.logging.default_handler)
-root_logger.setLevel(logging.INFO)
-
-logger = logging.getLogger(__name__)
-logger.info('Starting app')
-
 host = os.environ.get('HOST', '0.0.0.0')
 port = int(os.environ.get('PORT', 8000))
 debug = 'DEBUG' in os.environ
@@ -38,10 +27,22 @@ mouse_path = os.environ.get('MOUSE_PATH', '/dev/hidg1')
 # Keyboard layout on target computer.
 keyboard_layout = os.environ.get('KEYBOARD_LAYOUT', 'QWERTY')
 
-# Socket.io logs are too chatty at INFO level.
-if not debug:
+root_logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s %(name)-15s %(levelname)-4s %(message)s', '%Y-%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+root_logger.addHandler(flask.logging.default_handler)
+if debug:
+    root_logger.setLevel(logging.DEBUG)
+else:
+    root_logger.setLevel(logging.INFO)
+    # Socket.io logs are too chatty at INFO level.
     logging.getLogger('socketio').setLevel(logging.ERROR)
     logging.getLogger('engineio').setLevel(logging.ERROR)
+
+logger = logging.getLogger(__name__)
+logger.info('Starting app')
 
 app = flask.Flask(__name__, static_url_path='')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
