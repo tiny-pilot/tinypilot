88beb47e4b5b5611e0b354a100f358cd6a4fbeb2
Michael Lynch <mtlynch@users.noreply.github.com>
2020-12-18 16:19:16 -0500

    Add a dummy remote screen for development (#380)
    During development, TinyPilot doesn't have a real remote screen to display, so show a fake screen to help show realistic screen layout.

--- app/views.py
+++ app/views.py
@@ -9,3 +9,13 @@ views_blueprint = flask.Blueprint('views', __name__, url_prefix='')
 def index_get():
     return flask.render_template(
         'index.html', custom_elements_files=find_files.custom_elements_files())
+
+
+# On a real install, nginx redirects the /stream route to uStreamer, so a real
+# user should never hit this route in production. In development, show a fake
+# still image to give a better sense of how the TinyPilot UI looks.
+@views_blueprint.route('/stream', methods=['GET'])
+def stream_get():
+    if flask.current_app.debug:
+        return flask.send_file('tests/data/test-remote-screen.jpg')
+    return flask.abort(404)
