938cb455295911c60923cdf7cc6bb494cfba724d
Michael Lynch <mtlynch@users.noreply.github.com>
2020-12-22 10:07:44 -0500

    Refactor JSON responses in api.py (#392)

--- app/api.py
+++ app/api.py
@@ -9,27 +9,32 @@ api_blueprint = flask.Blueprint('api', __name__, url_prefix='/api')
 def shutdown_post():
     try:
         local_system.shutdown()
-        return flask.jsonify({
-            'success': True,
-            'error': None,
-        })
+        return _json_success()
     except local_system.Error as e:
-        return flask.jsonify({
-            'success': False,
-            'error': str(e),
-        }), 500
+        return _json_error(str(e)), 500
 
 
 @api_blueprint.route('/restart', methods=['POST'])
 def restart_post():
     try:
         local_system.restart()
-        return flask.jsonify({
-            'success': True,
-            'error': None,
-        })
+        return _json_success()
     except local_system.Error as e:
-        return flask.jsonify({
-            'success': False,
-            'error': str(e),
-        }), 500
+        return _json_error(str(e)), 500
+
+
+def _json_success(fields={}):
+    response = {
+        'success': True,
+        'error': None,
+    }
+    for k, v in fields.items():
+        response[k] = v
+    return flask.jsonify(response)
+
+
+def _json_error(message):
+    return flask.jsonify({
+        'success': False,
+        'error': message,
+    })
