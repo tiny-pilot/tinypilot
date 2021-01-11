31ad30f6ea117abe937e1a2758913281ed3d4e1e
tretinha <gustavo@tretinha.com>
2021-01-11 21:33:49 +0000

    Create new API endpoints
    The following endpoints were created:
    
    * /version
    * /latestRelease
    * /update

---
+++ app/update.py
@@ -0,0 +1,31 @@
+import logging
+import subprocess
+
+logger = logging.getLogger(__name__)
+
+
+class Error(Exception):
+    pass
+
+
+def update():
+    """Executes the TinyPilot update script.
+
+    The script takes 2~4 minutes to complete.
+
+    Returns:
+        True if successful.
+
+    Raises:
+        An Error object containing a str.
+        Example: Error('sudo: /opt/tinypilot-privileged/update: command not found').
+    """
+    logger.info('Updating TinyPilot')
+    result = subprocess.run(['sudo', '/opt/tinypilot-privileged/update'],
+                            capture_output=True,
+                            text=True)
+    try:
+        result.check_returncode()
+    except subprocess.CalledProcessError:
+        raise Error(result.stderr.strip())
+    return True
