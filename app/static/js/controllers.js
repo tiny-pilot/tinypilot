a33c3f08e9fbcf295ab1e3365fc80a58f7e367ac
Michael Lynch <git@mtlynch.io>
2021-01-22 12:50:10 -0500

    Refactor the logic in controllers.js
    The original implementation was pretty hacked together, so this refactors the code to be more reusable from other API controllers and makes abstraction boundaries more clear.

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -7,6 +7,46 @@
       .getAttribute("content");
   }
 
+  // Reads a response from an HTTP endpoint that we expect to contain a JSON
+  // body. Verifies the HTTP response was successful and the response type is
+  // JSON, but doesn't check anything beyond that.
+  function readHttpJsonResponse(response) {
+    const contentType = response.headers.get("content-type");
+    const isJson =
+      contentType && contentType.indexOf("application/json") !== -1;
+
+    // Success case is an HTTP 200 response and a JSON body.
+    if (response.status === 200 && isJson) {
+      return Promise.resolve(response.json());
+    }
+
+    // If this is JSON, try to read the error field.
+    if (isJson) {
+      return response.json().then((responseJson) => {
+        if (responseJson.hasOwnProperty("error")) {
+          return Promise.reject(new Error(responseJson.error));
+        }
+        return Promise.reject(new Error("Unknown error"));
+      });
+    }
+
+    // If all else fails, return the HTTP body as the error message.
+    return Promise.reject(new Error(response.statusText));
+  }
+
+  // Checks TinyPilot-level details of the response. The standard TinyPilot
+  // response body contains two fields: "success" (bool) and "error" (string)
+  // A message indicates success if success is true and error is non-null.
+  function checkJsonSuccess(response) {
+    if (result.hasOwnProperty("error") && response.error) {
+      return Promise.reject(new Error(response.error));
+    }
+    if (!result.hasOwnProperty("success") || !response.success) {
+      return Promise.reject(new Error("Unknown error"));
+    }
+    return Promise.resolve(response);
+  }
+
   function shutdown(restart) {
     let route = "/api/shutdown";
     if (restart) {
@@ -21,40 +61,28 @@
       cache: "no-cache",
       redirect: "error",
     })
-      .then((response) => {
+      .then((httpResponse) => {
         // A 502 usually means that nginx shutdown before it could process the
         // response. Treat this as success.
-        if (response.status === 502) {
+        if (httpResponse.status === 502) {
           return Promise.resolve({
             success: true,
             error: null,
           });
         }
-        if (response.status !== 200) {
-          // See if the error response is JSON.
-          const contentType = response.headers.get("content-type");
-          if (contentType && contentType.indexOf("application/json") !== -1) {
-            return response.json().then((data) => {
-              return Promise.reject(new Error(data.error));
-            });
-          }
-          return Promise.reject(new Error(response.statusText));
-        }
-        return response.json();
+        return readHttpJsonResponse(httpResponse);
       })
-      .then((result) => {
-        if (result.hasOwnProperty("success") && result.success) {
-          return Promise.resolve({});
-        }
-        if (result.hasOwnProperty("error") && result.error) {
-          return Promise.reject(new Error(result.error));
-        }
-        return Promise.reject(new Error("Unknown error"));
+      .then((jsonResponse) => {
+        return checkJsonSuccess(jsonResponse);
+      })
+      .then(() => {
+        // The shutdown API has no details, so return an empty dict.
+        return Promise.resolve({});
       })
       .catch((error) => {
-        // Depending on timing, the server may not respond to the shutdown request
-        // because it's shutting down. If we get a NetworkError, assume the
-        // shutdown succeeded.
+        // Depending on timing, the server may not respond to the shutdown
+        // request because it's shutting down. If we get a NetworkError, assume
+        // the shutdown succeeded.
         if (error.message.indexOf("NetworkError") >= 0) {
           return Promise.resolve({});
         }
