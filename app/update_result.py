8925736977301490c17892d335608a7c5af5fc3f
Michael Lynch <git@mtlynch.io>
2021-02-10 18:40:19 -0500

    Add docstrings to update_result

--- app/update_result.py
+++ app/update_result.py
@@ -13,6 +13,23 @@ class Result:
 
 
 def read(result_file):
+    """Reads an update result file.
+
+    Parses the contents of a result file into a Result object. The file should
+    have a format like:
+
+      {
+        "success": true,
+        "error": "",
+        "timestamp": "2021-02-10T085735Z",
+      }
+
+    Args:
+        result_file: A file containing JSON-formatted results of an update job.
+
+    Returns:
+        A Result object parsed from the file.
+    """
     raw_result = json.load(result_file, cls=_ResultDecoder)
     return Result(success=raw_result.get('success', False),
                   error=raw_result.get('error', ''),
@@ -21,6 +38,15 @@ def read(result_file):
 
 
 def write(result, result_file):
+    """Serializes a Result object to a file as JSON.
+
+    Args:
+        result: A Result object containing results of an update job.
+        result_file: File handle to which to serialize the result object.
+
+    Returns:
+        None.
+    """
     json.dump(dataclasses.asdict(result), result_file, cls=_ResultEncoder)
 
 
