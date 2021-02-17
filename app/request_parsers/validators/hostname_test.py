68e875ae9d8b3b68ef49d86ecfa6a297aa85c269
Jan Heuermann <jan@jotaen.net>
2021-02-17 22:22:21 +0100

    Simplify assertions

--- app/request_parsers/validators/hostname_test.py
+++ app/request_parsers/validators/hostname_test.py
@@ -6,24 +6,24 @@ from request_parsers.validators import hostname
 class MyTestCase(unittest.TestCase):
 
     def test_accepts_hostname_with_valid_characters(self):
-        self.assertEqual(hostname.validate('tiny-pilot123'), True)
+        self.assertTrue(hostname.validate('tiny-pilot123'))
 
     def test_accepts_hostname_with_exactly_63_characters(self):
-        self.assertEqual(hostname.validate('a' * 63), True)
+        self.assertTrue(hostname.validate('a' * 63))
 
     def test_rejects_hostname_that_is_not_a_string(self):
-        self.assertEqual(hostname.validate(1), False)
+        self.assertFalse(hostname.validate(1))
 
     def test_rejects_hostnames_with_invalid_characters(self):
-        self.assertEqual(hostname.validate('TINYPILOT'), False)
-        self.assertEqual(hostname.validate('tinypilot***'), False)
-        self.assertEqual(hostname.validate('tiny.pilot'), False)
+        self.assertFalse(hostname.validate('TINYPILOT'))
+        self.assertFalse(hostname.validate('tinypilot***'))
+        self.assertFalse(hostname.validate('tiny.pilot'))
 
     def test_rejects_empty_hostname(self):
-        self.assertEqual(hostname.validate(''), False)
+        self.assertFalse(hostname.validate(''))
 
     def test_rejects_hostname_that_is_longer_than_63_chars(self):
-        self.assertEqual(hostname.validate('a' * 64), False)
+        self.assertFalse(hostname.validate('a' * 64))
 
     def test_rejects_hostname_that_starts_with_a_dash(self):
-        self.assertEqual(hostname.validate('-invalid'), False)
+        self.assertFalse(hostname.validate('-invalid'))
