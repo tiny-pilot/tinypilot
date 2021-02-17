3ff98673cf63cdfafe275b533808ea5e8ca8760a
Jan Heuermann <jan@jotaen.net>
2021-02-17 18:54:31 +0100

    Fix method invocation (pass list instead of string)

--- app/request_parsers/hostname.py
+++ app/request_parsers/hostname.py
@@ -2,5 +2,5 @@ from request_parsers import message as message_parser
 
 
 def parse_hostname(request):
-    message = message_parser.parse_message(request, 'hostname')
+    message = message_parser.parse_message(request, ['hostname'])
     return message['hostname']
