2d231b23c122454cdc8867bb3e72fc12a7a1cf02
Jan Heuermann <jan@jotaen.net>
2021-03-02 15:50:23 +0100

    Always timeout polling, not just on network errors

--- app/static/js/util/poll.js
+++ app/static/js/util/poll.js
@@ -1,41 +1,33 @@
 /**
- * Calls a function repetitively until either the result is valid or the
- * maximum number of network errors is reached.
+ * Invokes a Promise repetitively until it resolves to the expected outcome.
  *
  * @template T
  * @param {function(): Promise<T>} fn The function to be polled.
  * @param {function(T): boolean} validate Function to validate fn’s result.
- * @param {number} interval Interval in milliseconds.
- * @param {number} maxConsecutiveNetworkErrors The maximum number of network
- *     errors after which the polling should stop.
+ * @param {number} interval Polling interval in milliseconds.
+ * @param {number} [timeout] Time in milliseconds after which to abort.
  * @returns {Promise<T>}
  */
-async function poll({ fn, validate, interval, maxConsecutiveNetworkErrors }) {
-  let result = null;
-  let consecutiveNetworkErrors = 0;
+async function poll({ fn, validate, interval, timeout }) {
+  const start = new Date().getTime();
+  let lastError = null;
 
   const executePoll = async (resolve, reject) => {
     try {
-      result = await fn();
-      consecutiveNetworkErrors = 0;
-    } catch (error) {
-      result = null;
-      if (error.message.includes("NetworkError")) {
-        consecutiveNetworkErrors++;
-        if (
-          maxConsecutiveNetworkErrors &&
-          consecutiveNetworkErrors === maxConsecutiveNetworkErrors
-        ) {
-          return reject(error);
-        }
+      const result = await fn();
+      if (validate(result)) {
+        return resolve(result);
+      } else {
+        lastError = new Error("Result did not pass validation");
       }
+    } catch (error) {
+      lastError = error;
     }
-
-    if (validate(result)) {
-      return resolve(result);
-    } else {
-      setTimeout(executePoll, interval, resolve, reject);
+    const totalTimeElapsed = new Date().getTime() - start;
+    if (timeout !== undefined && totalTimeElapsed > timeout) {
+      return reject(lastError);
     }
+    setTimeout(executePoll, interval, resolve, reject);
   };
 
   return new Promise(executePoll);
