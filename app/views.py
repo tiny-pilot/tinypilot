9a405a1484b64e1ec4062cbd276cc32ad6a1cb43
Jan Heuermann <jan@jotaen.net>
2021-03-05 18:57:09 +0100

    Setup internal style guide

--- app/views.py
+++ app/views.py
@@ -11,6 +11,15 @@ def index_get():
         'index.html', custom_elements_files=find_files.custom_elements_files())
 
 
+# The style guide is for development purpose only, so we don’t ship it to
+# end users.
+@views_blueprint.route('/styleguide', methods=['GET'])
+def styleguide_get():
+    if flask.current_app.debug:
+        return flask.render_template('styleguide.html')
+    return flask.abort(404)
+
+
 # On a real install, nginx redirects the /stream route to uStreamer, so a real
 # user should never hit this route in production. In development, show a fake
 # still image to give a better sense of how the TinyPilot UI looks.
