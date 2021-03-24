4aa638132bd3b2954f5241798e6fe5061969fff8
Jan Heuermann <jan@jotaen.net>
2021-03-24 12:33:58 +0100

    Improve docblock description

--- app/json_response.py
+++ app/json_response.py
@@ -4,15 +4,15 @@ import flask
 # The default dictionary is okay because we're not modifying it.
 # pylint: disable=dangerous-default-value
 def success(fields={}):
-    """A JSON-API response indicating a successful request.
+    """A JSON response for a successful request.
+
     Args:
         fields: Dictionary with JSON properties to include.
 
     Returns:
-        A `flask.Response` object with JSON body. The JSON structure always
-        contains a `success` property (which is `true`) and an `error` property
-        (which is `null`). There may appear additional endpoint-dependent
-        properties.
+        A `flask.Response` object with JSON body. The JSON structure contains a
+        `success` property (which is `true`) and an `error` property (which is
+        `null`). The additional fields given are merged into the JSON output.
     """
     response = {
         'success': True,
@@ -24,14 +24,15 @@ def success(fields={}):
 
 
 def error(message):
-    """A JSON-API response indicating a failed request.
+    """A JSON response for a failed request.
+
     Args:
         message: String containing the error message.
 
     Returns:
-        A `flask.Response` object with JSON body. The JSON structure always
-        contains a `success` property (which is `false`) and an `error` property
-        (which is a string containing the error message).
+        A `flask.Response` object with JSON body. The JSON structure contains a
+        `success` property (which is `false`) and an `error` property (which is
+        a string containing the error message).
     """
     return flask.jsonify({
         'success': False,
