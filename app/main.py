57676a23ec29052c99796f2e42166328c3f5557c
Michael Lynch <mtlynch@users.noreply.github.com>
2021-04-01 11:56:50 -0400

    Disable hot reloading by default (#626)
    I kept it in to facilitate development, but it's generating noise in the logs during updates when TinyPilot tries to reload as files change for the update. Now it's off by default, but the serve-dev script enables it for development.

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ from find_files import find as find_files
 host = os.environ.get('HOST', '127.0.0.1')
 port = int(os.environ.get('PORT', 8000))
 debug = 'DEBUG' in os.environ
-use_reloader = os.environ.get('USE_RELOADER', '1') == '1'
+use_reloader = os.environ.get('USE_RELOADER', '0') == '1'
 
 root_logger = logging.getLogger()
 handler = logging.StreamHandler()
