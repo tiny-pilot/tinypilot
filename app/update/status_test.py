461c07952790179302b56584870974f4d8b17e1b
Michael Lynch <mtlynch@users.noreply.github.com>
2021-04-14 11:54:48 -0400

    Add missing __init__.py files to directories where they're absent (#644)
    This also fixes some bugs in tests due to incorrect module traversal.

--- app/update/status_test.py
+++ app/update/status_test.py
@@ -7,8 +7,8 @@ import update.status
 
 class StatusTest(unittest.TestCase):
 
-    @mock.patch.object(update.subprocess, 'check_output')
-    @mock.patch.object(update.status.result_reader, 'read')
+    @mock.patch.object(update.status.subprocess, 'check_output')
+    @mock.patch.object(update.status.update.result_reader, 'read')
     def test_returns_not_running_when_there_is_no_process_nor_result_file(
             self, mock_read_update_result, mock_check_output):
         mock_check_output.return_value = """
@@ -18,12 +18,12 @@ root        51  0.0  0.0 103152 21264 ?        Ss   Apr03   0:00 /lib/dummy-b
 """.lstrip().encode('utf-8')
         mock_read_update_result.return_value = None
 
-        status_actual, error_actual = update.get()
-        self.assertEqual(update.Status.NOT_RUNNING, status_actual)
+        status_actual, error_actual = update.status.get()
+        self.assertEqual(update.status.Status.NOT_RUNNING, status_actual)
         self.assertIsNone(error_actual)
 
-    @mock.patch.object(update.subprocess, 'check_output')
-    @mock.patch.object(update.status.result_reader, 'read')
+    @mock.patch.object(update.status.subprocess, 'check_output')
+    @mock.patch.object(update.status.update.result_reader, 'read')
     def test_returns_in_progress_when_update_process_is_running(
             self, mock_read_update_result, mock_check_output):
         mock_check_output.return_value = """
@@ -33,12 +33,12 @@ root        51  0.0  0.0 103152 21264 ?        Ss   Apr03   0:00 /opt/tinypilot-
 """.lstrip().encode('utf-8')
         mock_read_update_result.return_value = None
 
-        status_actual, error_actual = update.get()
-        self.assertEqual(update.Status.IN_PROGRESS, status_actual)
+        status_actual, error_actual = update.status.get()
+        self.assertEqual(update.status.Status.IN_PROGRESS, status_actual)
         self.assertIsNone(error_actual)
 
-    @mock.patch.object(update.subprocess, 'check_output')
-    @mock.patch.object(update.status.result_reader, 'read')
+    @mock.patch.object(update.status.subprocess, 'check_output')
+    @mock.patch.object(update.status.update.result_reader, 'read')
     def test_ignores_update_result_if_update_is_running(self,
                                                         mock_read_update_result,
                                                         mock_check_output):
@@ -53,12 +53,12 @@ root        51  0.0  0.0 103152 21264 ?        Ss   Apr03   0:00 /opt/tinypilot-
         mock_read_update_result.return_value = update.result.Result(
             error=None, timestamp='2021-02-10T085735Z')
 
-        status_actual, error_actual = update.get()
-        self.assertEqual(update.Status.IN_PROGRESS, status_actual)
+        status_actual, error_actual = update.status.get()
+        self.assertEqual(update.status.Status.IN_PROGRESS, status_actual)
         self.assertIsNone(error_actual)
 
-    @mock.patch.object(update.subprocess, 'check_output')
-    @mock.patch.object(update.status.result_reader, 'read')
+    @mock.patch.object(update.status.subprocess, 'check_output')
+    @mock.patch.object(update.status.update.result_reader, 'read')
     def test_returns_success_when_no_process_is_running_and_last_run_was_ok(
             self, mock_read_update_result, mock_check_output):
         mock_check_output.return_value = """
@@ -69,12 +69,12 @@ root        51  0.0  0.0 103152 21264 ?        Ss   Apr03   0:00 /lib/dummy-b
         mock_read_update_result.return_value = update.result.Result(
             error=None, timestamp='2021-02-10T085735Z')
 
-        status_actual, error_actual = update.get()
-        self.assertEqual(update.Status.DONE, status_actual)
+        status_actual, error_actual = update.status.get()
+        self.assertEqual(update.status.Status.DONE, status_actual)
         self.assertIsNone(error_actual)
 
-    @mock.patch.object(update.subprocess, 'check_output')
-    @mock.patch.object(update.status.result_reader, 'read')
+    @mock.patch.object(update.status.subprocess, 'check_output')
+    @mock.patch.object(update.status.update.result_reader, 'read')
     def test_returns_error_when_no_process_is_running_and_last_run_had_error(
             self, mock_read_update_result, mock_check_output):
         mock_check_output.return_value = """
@@ -85,6 +85,6 @@ root        51  0.0  0.0 103152 21264 ?        Ss   Apr03   0:00 /lib/dummy-b
         mock_read_update_result.return_value = update.result.Result(
             error='dummy update error', timestamp='2021-02-10T085735Z')
 
-        status_actual, error_actual = update.get()
-        self.assertEqual(update.Status.DONE, status_actual)
+        status_actual, error_actual = update.status.get()
+        self.assertEqual(update.status.Status.DONE, status_actual)
         self.assertEqual('dummy update error', error_actual)
