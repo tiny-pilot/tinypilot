85476aecbda0b821fdceeba21370e640ecfee945
Jason Wallace <jdeanwallace@gmail.com>
2021-07-19 16:12:02 +0200

    Change update_logs streaming strategy.

--- app/update_logs.py
+++ app/update_logs.py
@@ -1,12 +1,12 @@
 import logging
+import os.path
 import subprocess
-import tempfile
 
 import eventlet
 import flask_socketio
+from flask import session
 
 _READ_SCRIPT_PATH = '/opt/tinypilot-privileged/read-update-log'
-_STREAM_SCRIPT_PATH = '/opt/tinypilot-privileged/stream-update-log'
 logger = logging.getLogger(__name__)
 
 
@@ -42,58 +42,22 @@ def read():
     return output
 
 
-def stream(file):
-    """Stream the TinyPilot update logs to a file.
-
-    Args:
-        file: The file to which stdout & stderr will be written.
-
-    Returns:
-        A Popen instance of stream-update-log process.
-    """
-    logger.info('Running stream-update-log')
-    process = subprocess.Popen(['sudo', _STREAM_SCRIPT_PATH],
-                               bufsize=0,
-                               stderr=subprocess.STDOUT,
-                               stdout=file)
-    logger.info('stream-update-log completed successfully')
-    return process
-
-
 class Namespace(flask_socketio.Namespace):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.writer = None
-        self.reader = None
-        self.process = None
-
     def on_connect(self):
-        logger.info('connected to update_logs namespace')
-        # We can't use in-memory text streams here (i.e. io.StringIO) because
-        # Popen requires a fileno.
-        # We can't PIPE to stdout because Popen.stdout.read is blocking.
-        # We need separate read & write handles on the same file to maintain
-        # independent reading & writing positions.
-        self.writer = tempfile.NamedTemporaryFile()
-        self.reader = open(self.writer.name)
-        self.process = stream(file=self.writer)
+        session['update_logs'] = ''
 
     def on_disconnect(self):
-        logger.info('disconnected fron update_logs namespace')
-        self.process.kill()
-        self.reader.close()
-        self.writer.close()
+        del session['update_logs']
 
     def on_read(self):
         logger.info('read event in update_logs namespace')
-        while True:
-            try:
-                data = self.reader.read()
-            except ValueError:
-                # ValueError: I/O operation on closed file.
-                break
-            if data:
-                logger.info('data: %s', data)
-                flask_socketio.emit('read_response', data)
-            eventlet.sleep(0)
+        while 'update_logs' in session:
+            logs = read()
+            common_logs = os.path.commonprefix([session['update_logs'], logs])
+            new_logs = logs[len(common_logs):]
+            if new_logs:
+                logger.info('new_logs: %s', new_logs)
+                flask_socketio.emit('read_response', new_logs)
+                session['update_logs'] = logs
+            eventlet.sleep(0.5)
