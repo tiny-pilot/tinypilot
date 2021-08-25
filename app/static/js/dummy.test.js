a42aa8906f0490773ce083cf3c4656a34421bc9c
Jan Heuermann <jan@jotaen.net>
2021-08-25 12:49:48 +0200

    Setup unit tests with Mocha

---
+++ app/static/js/dummy.test.js
@@ -0,0 +1,5 @@
+// Dummy test file, because otherwise Mocha complains that there are no tests.
+// We currently don’t have unit tests in the Community repo, but we still want
+// to have a unified setup with Pro.
+
+// TODO(jotaen) Remove this once we add the first test in Community.
