baffcb90fcaa573a5437948fa025dac5f6eee094
Jason Wallace <jdeanwallace@gmail.com>
2021-08-26 17:21:20 +0200

    Accurately detect HID failures. (#767)
    * Add HID failure detection.
    
    * PR changes.
    
    * PR changes.

--- app/hid/write.py
+++ app/hid/write.py
@@ -1,5 +1,7 @@
+import dataclasses
 import logging
 import multiprocessing
+import typing
 
 logger = logging.getLogger(__name__)
 
@@ -10,6 +12,51 @@ class Error(Exception):
 
 class WriteError(Error):
     pass
+
+
+@dataclasses.dataclass
+class ProcessResult:
+    return_value: typing.Any = None
+    exception: Exception = None
+
+    def was_successful(self) -> bool:
+        return self.exception is None
+
+
+class ProcessWithResult(multiprocessing.Process):
+    """A multiprocessing.Process object that keeps track of the child process'
+    result (i.e., the return value and exception raised).
+
+    Inspired by:
+    https://stackoverflow.com/a/33599967/3769045
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Create the Connection objects used for communication between the
+        # parent and child processes.
+        self.parent_conn, self.child_conn = multiprocessing.Pipe()
+
+    def run(self):
+        """Method to be run in sub-process."""
+        result = ProcessResult()
+        try:
+            if self._target:
+                result.return_value = self._target(*self._args, **self._kwargs)
+        except Exception as e:
+            result.exception = e
+            raise
+        finally:
+            self.child_conn.send(result)
+
+    def result(self):
+        """Get the result from the child process.
+
+        Returns:
+            If the child process has completed, a ProcessResult object.
+            Otherwise, a None object.
+        """
+        return self.parent_conn.recv() if self.parent_conn.poll() else None
 
 
 def _write_to_hid_interface_immediately(hid_path, buffer):
@@ -31,17 +78,18 @@ def write_to_hid_interface(hid_path, buffer):
     # Writes can hang, for example, when TinyPilot is attempting to write to the
     # mouse interface, but the target system has no GUI. To avoid locking up the
     # main server process, perform the HID interface I/O in a separate process.
-    write_process = multiprocessing.Process(
+    write_process = ProcessWithResult(
         target=_write_to_hid_interface_immediately,
         args=(hid_path, buffer),
         daemon=True)
     write_process.start()
     write_process.join(timeout=0.5)
-    # If the process is still alive, it means the write failed to complete in
-    # time.
     if write_process.is_alive():
         write_process.kill()
         _wait_for_process_exit(write_process)
+    result = write_process.result()
+    # If the result is None, it means the write failed to complete in time.
+    if result is None or not result.was_successful():
         raise WriteError(
             'Failed to write to HID interface: %s. Is USB cable connected?' %
             hid_path)
---
+++ app/hid/write_test.py
@@ -0,0 +1,67 @@
+import io
+import time
+import unittest
+from unittest import mock
+
+import hid.write
+
+
+class WriteTest(unittest.TestCase):
+
+    def test_process_with_result_child_completed(self):
+
+        def target():
+            pass
+
+        process = hid.write.ProcessWithResult(target=target, daemon=True)
+        process.start()
+        process.join()
+        result = process.result()
+        self.assertTrue(result.was_successful())
+        self.assertEqual(
+            hid.write.ProcessResult(return_value=None, exception=None), result)
+
+    def test_process_with_result_child_not_completed(self):
+
+        def target():
+            time.sleep(1)
+
+        process = hid.write.ProcessWithResult(target=target, daemon=True)
+        process.start()
+        # Get the result before the child process has completed.
+        self.assertIsNone(process.result())
+
+        # Clean up the running child process.
+        process.kill()
+
+    def test_process_with_result_child_exception(self):
+
+        def target():
+            raise Exception('Child exception')
+
+        # Silence stderr while the child exception is being raised to avoid
+        # polluting the terminal output.
+        with mock.patch('sys.stderr', io.StringIO()):
+            process = hid.write.ProcessWithResult(target=target, daemon=True)
+            process.start()
+            process.join()
+        result = process.result()
+        self.assertFalse(result.was_successful())
+        self.assertEqual(
+            hid.write.ProcessResult(return_value=None, exception=mock.ANY),
+            result)
+        self.assertEqual('Child exception', str(result.exception))
+
+    def test_process_with_result_return_value(self):
+
+        def target():
+            return 'Done!'
+
+        process = hid.write.ProcessWithResult(target=target, daemon=True)
+        process.start()
+        process.join()
+        result = process.result()
+        self.assertTrue(result.was_successful())
+        self.assertEqual(
+            hid.write.ProcessResult(return_value='Done!', exception=None),
+            result)
