3c875f53d1d8a270d4d9ed028f54282847157c04
Jan Heuermann <jan@jotaen.net>
2021-12-02 17:06:29 +0100

    Add test case

--- app/static/js/logs.test.js
+++ app/static/js/logs.test.js
@@ -3,6 +3,17 @@ import assert from "assert";
 import { filterSensitiveData } from "./logs.js";
 
 describe("filterSensitiveData", () => {
+  it("returns the logs as is if there is no sensitive data", () => {
+    const logTextWithoutSensitiveData = `
+2020-01-01T00:00:01Z INFO Something happened
+2020-01-01T00:00:02Z INFO Some other thing happened
+2020-01-01T00:00:03Z ERROR Something else happened
+2020-01-01T00:00:04Z INFO Some regular message
+`;
+    const filteredLogs = filterSensitiveData(logTextWithoutSensitiveData);
+    assert.strictEqual(filteredLogs, logTextWithoutSensitiveData);
+  });
+
   it("removes all lines that are flagged as sensitive", () => {
     const filteredLogs = filterSensitiveData(`
 2020-01-01T00:00:01Z INFO Something happened
