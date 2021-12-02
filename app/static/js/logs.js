fe6ef3774f9a38d63575478fdddef902ac92ccbe
Jan Heuermann <jan@jotaen.net>
2021-12-02 12:37:54 +0100

    Extract filter function for log messages

---
+++ app/static/js/logs.js
@@ -0,0 +1,34 @@
+const NEWLINE = "\n";
+const SENSITIVE_MARKER_START = "[SENSITIVE]";
+const SENSITIVE_MARKER_END = "[/SENSITIVE]";
+
+/**
+ * Filters out all log lines that are flagged as sensitive. That includes:
+ * - the entire first line that contains the `[SENSITIVE]` start marker
+ * - the entire last line that contains the `[/SENSITIVE]` end marker
+ * - all lines in between those markers
+ *
+ * This function doesn’t make any other assumptions on how the log lines are
+ * formatted.
+ * @param logText {string}
+ */
+export function filterSensitiveData(logText) {
+  // Note: this implementation is not perfectly accurate in regards to certain
+  // edge cases (e.g. multiple redundant markers on the same line). For our
+  // purposes, it’s good enough, though.
+  let isWithinMarkers = false;
+  return logText
+    .split(NEWLINE)
+    .filter((logLine) => {
+      const containsStartMarker = logLine.includes(SENSITIVE_MARKER_START);
+      const containsEndMarker = logLine.includes(SENSITIVE_MARKER_END);
+      if (containsStartMarker) {
+        isWithinMarkers = true;
+      }
+      if (containsEndMarker) {
+        isWithinMarkers = false;
+      }
+      return !(containsStartMarker || containsEndMarker || isWithinMarkers);
+    })
+    .join(NEWLINE);
+}
---
+++ app/static/js/logs.test.js
@@ -0,0 +1,25 @@
+import { describe, it } from "mocha";
+import assert from "assert";
+import { filterSensitiveData } from "./logs.js";
+
+describe("filterSensitiveData", () => {
+  it("removes all lines that are flagged as sensitive", () => {
+    const filteredLogs = filterSensitiveData(`
+2020-01-01T00:00:01Z INFO Something happened
+2020-01-01T00:00:02Z INFO Some other thing happened
+2020-01-01T00:00:03Z INFO [SENSITIVE] This message contains sensitive data [/SENSITIVE]
+2020-01-01T00:00:04Z ERROR Something else happened
+2020-01-01T00:00:05Z DEBUG [SENSITIVE] That’s a secret message
+which spans multiple lines
+of the log text [/SENSITIVE]
+2020-01-01T00:00:06Z INFO Some regular message
+`);
+    const expectedOutput = `
+2020-01-01T00:00:01Z INFO Something happened
+2020-01-01T00:00:02Z INFO Some other thing happened
+2020-01-01T00:00:04Z ERROR Something else happened
+2020-01-01T00:00:06Z INFO Some regular message
+`;
+    assert.strictEqual(filteredLogs, expectedOutput);
+  });
+});
