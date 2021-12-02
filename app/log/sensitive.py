b1a509dee61c9a6a3e2d5368c9902276da4e2cb5
Jan Heuermann <jan@jotaen.net>
2021-12-02 10:54:16 +0100

    Comment phrasing

--- app/log/sensitive.py
+++ app/log/sensitive.py
@@ -4,10 +4,10 @@ import logging
 class Logger(logging.getLoggerClass()):
     """A logger with additional methods for flagging log data as sensitive.
 
-    Use these sensitive log messages whenever the log message might contain
+    Use these sensitive log methods whenever the log message might contain
     security-related information (even if it’s just fragments), such as secrets,
     personal data, or anything else that a user likely doesn’t want to be
-    revealed when sharing the logs for trouble-shooting purposes.
+    revealed when sharing the logs for troubleshooting purposes.
     """
 
     def __init__(self, name):
