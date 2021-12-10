831410752743c40b2e9012f90fd4e12bea6f8f9a
Erwann <erwann.gentric@gmail.com>
2021-12-10 12:15:00 +0100

    Rework according to review

--- app/request_parsers/hostname_test.py
+++ app/request_parsers/hostname_test.py
@@ -16,16 +16,16 @@ class HostnameValidationTest(unittest.TestCase):
     def test_accepts_hostname_with_valid_characters(self):
         hostname_valid = 'tiny-pilot123'
         self.assertEqual(
+            hostname_valid,
             hostname.parse_hostname(
-                make_mock_request({'hostname': hostname_valid})),
-            hostname_valid)
+                make_mock_request({'hostname': hostname_valid})))
 
     def test_accepts_hostname_with_exactly_63_characters(self):
         hostname_63_chars = 'a' * 63
         self.assertEqual(
+            hostname_63_chars,
             hostname.parse_hostname(
-                make_mock_request({'hostname': hostname_63_chars})),
-            hostname_63_chars)
+                make_mock_request({'hostname': hostname_63_chars})))
 
     def test_rejects_hostname_that_is_not_a_string(self):
         with self.assertRaises(errors.InvalidHostnameError):
@@ -34,11 +34,15 @@ class HostnameValidationTest(unittest.TestCase):
             hostname.parse_hostname(make_mock_request({'hostname': None}))
 
     def test_rejects_hostnames_with_invalid_characters(self):
-        for value in ['TINYPILOT', 'tinypilot***', 'tiny.pilot']:
-            with self.subTest(value):
-                with self.assertRaises(errors.InvalidHostnameError):
-                    hostname.parse_hostname(
-                        make_mock_request({'hostname': value}))
+        with self.assertRaises(errors.InvalidHostnameError):
+            hostname.parse_hostname(make_mock_request({'hostname': 'TINYPILOT'
+                                                      }))
+        with self.assertRaises(errors.InvalidHostnameError):
+            hostname.parse_hostname(
+                make_mock_request({'hostname': 'tinypilot***'}))
+        with self.assertRaises(errors.InvalidHostnameError):
+            hostname.parse_hostname(
+                make_mock_request({'hostname': 'tiny.pilot'}))
 
     def test_rejects_localhost_as_hostname(self):
         with self.assertRaises(errors.InvalidHostnameError):
