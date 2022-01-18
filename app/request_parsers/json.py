8be491d62e6cb72c5f775fe1270c0fde87d1878a
Erwann <erwann.gentric@gmail.com>
2022-01-18 12:23:48 +0100

    Rename message with json

--- app/request_parsers/json.py
+++ app/request_parsers/json.py
@@ -2,27 +2,27 @@ from request_parsers import errors
 
 
 def parse_json_body(request, required_fields):
-    """Parse a message with a JSON payload.
+    """Parse a request with a JSON payload.
 
     Args:
       request: A flask.Request object that caller expects to carry a JSON
         dictionary payload.
-      required_fields: A list of required fields in the message dictionary.
+      required_fields: A list of required fields in the JSON payload.
 
     Returns:
       The parsed field values as tuple. The arity and order of the tuple
       matches that of the `required_fields` argument.
     """
-    message = request.get_json()
+    json = request.get_json()
 
-    if not isinstance(message, dict):
+    if not isinstance(json, dict):
         raise errors.MalformedRequestError(
             'Request is invalid, expecting a JSON dictionary')
 
     result = []
     for field in required_fields:
-        if field not in message:
+        if field not in json:
             raise errors.MissingFieldError('Missing required field: %s' % field)
-        result.append(message[field])
+        result.append(json[field])
 
     return tuple(result)
