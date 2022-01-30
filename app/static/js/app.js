3ff070b0f3c3fceb1ea7aa8483bdae502108a097
Jan Heuermann <jan@jotaen.net>
2022-01-30 13:58:12 +0100

    Add “Wake on LAN” menu item

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -340,6 +340,9 @@ menuBar.addEventListener("debug-logs-dialog-requested", () => {
 menuBar.addEventListener("mass-storage-dialog-requested", () => {
   document.getElementById("feature-pro-overlay").show();
 });
+menuBar.addEventListener("wake-on-lan-dialog-requested", () => {
+  document.getElementById("feature-pro-overlay").show();
+});
 menuBar.addEventListener("video-settings-dialog-requested", () => {
   document.getElementById("video-settings-dialog").getSettings();
   document.getElementById("video-settings-overlay").show();
