a1311e5dfc6ad511f5c2acaddd2d547f21b7dc30
Jan Heuermann <jan@jotaen.net>
2022-03-28 21:49:43 +0200

    Read config from settings file

--- app/views.py
+++ app/views.py
@@ -13,8 +13,8 @@ _DEFAULT_HOSTNAME = 'tinypilot'
 def index_get():
     return flask.render_template(
         'index.html',
-        # TODO(jotaen) Get this from setting file:
-        use_webrtc_remote_screen=False,
+        use_webrtc_remote_screen=flask.current_app.config.get(
+            'USE_WEBRTC_REMOTE_SCREEN', False),
         page_title_prefix=_page_title_prefix(),
         custom_elements_files=find_files.custom_elements_files())
 
