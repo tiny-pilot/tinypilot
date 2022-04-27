33ad6ed397198fd6c2a02aa154a0e257df07aeb9
Michael Lynch <git@mtlynch.io>
2022-04-27 17:51:00 -0400

    work in progress

--- app/fetch_license.py
+++ app/fetch_license.py
@@ -5,14 +5,25 @@ blueprint = flask.Blueprint('py_license', __name__, url_prefix='/py-license')
 
 @blueprint.route('/tinypilot', methods=['GET'])
 def tinypilot_license_get():
-    response = flask.make_response(_read_file('LICENSE'), 200)
-    response.mimetype = 'text/plain'
-    return response
+    return _make_plaintext_response('LICENSE')
 
 
 @blueprint.route('/flask', methods=['GET'])
 def flask_license_get():
     return 'DUMMY LICENSE'
+
+
+@blueprint.route('/flask-socketio', methods=['GET'])
+def flask_socketio_license_get():
+    return _make_plaintext_response(
+        'venv/lib/python3.7/site-packages/Flask_SocketIO-5.0.1.dist-info/LICENSE'
+    )
+
+
+def _make_plaintext_response(license_path):
+    response = flask.make_response(_read_file(license_path), 200)
+    response.mimetype = 'text/plain'
+    return response
 
 
 def _read_file(license_path):
