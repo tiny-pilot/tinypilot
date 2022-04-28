2f24256b1ef93e8c8eb0b5ec4918534a8d84596e
Michael Lynch <git@mtlynch.io>
2022-04-28 18:13:59 -0400

    Add more licenses

--- app/fetch_license.py
+++ app/fetch_license.py
@@ -3,14 +3,44 @@ import glob
 import flask
 
 _LICENSE_PATTERNS = {
-    'flask-socketio':
-        'venv/lib/python3.7/site-packages/Flask_SocketIO-*.dist-info/LICENSE',
-    'Flask-WTF':
-        'venv/lib/python3.7/site-packages/Flask_WTF-*.dist-info/LICENSE',
     'python':
         '/usr/lib/python3.7/LICENSE.txt',
     'tinypilot':
         'LICENSE',
+    'eventlet':
+        'venv/lib/python3.7/site-packages/eventlet-*.dist-info/LICENSE*',
+    'Flask':
+        'venv/lib/python3.7/site-packages/Flask-*.dist-info/LICENSE*',
+    'Flask-SocketIO':
+        'venv/lib/python3.7/site-packages/Flask_SocketIO-*.dist-info/LICENSE*',
+    'Flask-WTF':
+        'venv/lib/python3.7/site-packages/Flask_WTF-*.dist-info/LICENSE*',
+    # pyyaml's PyPI package does not include a license, so we skip pyyaml.
+    'bidict':
+        'venv/lib/python3.7/site-packages/bidict-*.dist-info/LICENSE*',
+    'click':
+        'venv/lib/python3.7/site-packages/click-*.dist-info/LICENSE*',
+    'dnspython':
+        'venv/lib/python3.7/site-packages/dnspython-*.dist-info/LICENSE*',
+    'greenlet':
+        'venv/lib/python3.7/site-packages/greenlet-*.dist-info/LICENSE*',
+    'itsdangerous':
+        'venv/lib/python3.7/site-packages/itsdangerous-*.dist-info/LICENSE*',
+    'Jinja2':
+        'venv/lib/python3.7/site-packages/Jinja2-*.dist-info/LICENSE*',
+    'MarkupSafe':
+        'venv/lib/python3.7/site-packages/MarkupSafe-*.dist-info/LICENSE*',
+    # monotonic's PyPI package does not include a license, so we skip monotonic.
+    'python-engineio':
+        'venv/lib/python3.7/site-packages/python_engineio-*.dist-info/LICENSE*',
+    'python-socketio':
+        'venv/lib/python3.7/site-packages/python_socketio-*.dist-info/LICENSE*',
+    'six':
+        'venv/lib/python3.7/site-packages/six-*.dist-info/LICENSE*',
+    'Werkzeug':
+        'venv/lib/python3.7/site-packages/Werkzeug-*.dist-info/LICENSE*',
+    'WTForms':
+        'venv/lib/python3.7/site-packages/WTForms-*.dist-info/LICENSE*',
 }
 
 blueprint = flask.Blueprint('py_license', __name__, url_prefix='/py-license')
