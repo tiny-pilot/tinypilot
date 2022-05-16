bd42867c5adbc8cfdb1543959a2e4c8a77cb9e75
Michael Lynch <git@mtlynch.io>
2022-05-16 17:10:40 -0400

    Handle license URLs in Python

--- app/fetch_license.py
+++ app/fetch_license.py
@@ -2,48 +2,76 @@ import glob
 
 import flask
 
-_LICENSE_PATTERNS = {
+# pylint: disable=line-too-long
+_LICENSE_FILE_GLOB_PATTERNS = {
     'tinypilot':
-        'LICENSE',
-    'python':
+        './LICENSE',
+    'Python':
         '/usr/lib/python3.7/LICENSE.txt',
+    'uStreamer':
+        'https://raw.githubusercontent.com/tiny-pilot/ustreamer/v4.13/LICENSE',
+    'nginx':
+        'https://nginx.org/LICENSE',
+    'Ansible':
+        'https://raw.githubusercontent.com/ansible/ansible/v2.9.10/COPYING',
+    'Janus':
+        'https://raw.githubusercontent.com/tiny-pilot/janus-gateway/v1.0.0/COPYING',
+    # JavaScript dependencies.
+    'webrtc-adapter':
+        'https://github.com/webrtcHacks/adapter/blob/18a8b4127cbc1376320cac5742d817b5b7dd0085/LICENSE.md',
+    'socket.io':
+        'https://github.com/socketio/socket.io/blob/3.1.2/LICENSE',
+
+    # From requirements.txt
     'eventlet':
-        'venv/lib/python3.7/site-packages/eventlet-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/eventlet-*.dist-info/LICENSE*',
     'Flask':
-        'venv/lib/python3.7/site-packages/Flask-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/Flask-*.dist-info/LICENSE*',
     'Flask-SocketIO':
-        'venv/lib/python3.7/site-packages/Flask_SocketIO-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/Flask_SocketIO-*.dist-info/LICENSE*',
     'Flask-WTF':
-        'venv/lib/python3.7/site-packages/Flask_WTF-*.dist-info/LICENSE*',
-    # pyyaml's PyPI package does not include a license, so we skip pyyaml.
+        './venv/lib/python3.7/site-packages/Flask_WTF-*.dist-info/LICENSE*',
+    # pyyaml's PyPI package does not include a license, so we have to link to
+    # the Github version.
+    'pyyaml':
+        'https://raw.githubusercontent.com/yaml/pyyaml/5.4.1/LICENSE',
     'bidict':
-        'venv/lib/python3.7/site-packages/bidict-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/bidict-*.dist-info/LICENSE*',
     'click':
-        'venv/lib/python3.7/site-packages/click-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/click-*.dist-info/LICENSE*',
     'dnspython':
-        'venv/lib/python3.7/site-packages/dnspython-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/dnspython-*.dist-info/LICENSE*',
     'greenlet':
-        'venv/lib/python3.7/site-packages/greenlet-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/greenlet-*.dist-info/LICENSE*',
     'itsdangerous':
-        'venv/lib/python3.7/site-packages/itsdangerous-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/itsdangerous-*.dist-info/LICENSE*',
     'Jinja2':
-        'venv/lib/python3.7/site-packages/Jinja2-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/Jinja2-*.dist-info/LICENSE*',
     'MarkupSafe':
-        'venv/lib/python3.7/site-packages/MarkupSafe-*.dist-info/LICENSE*',
-    # monotonic's PyPI package does not include a license, so we skip monotonic.
+        './venv/lib/python3.7/site-packages/MarkupSafe-*.dist-info/LICENSE*',
+    # monotonic's PyPI package does not include a license, so we have to link to
+    # the Github version.
+    'monotonic':
+        'https://github.com/atdt/monotonic',
     'python-engineio':
-        'venv/lib/python3.7/site-packages/python_engineio-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/python_engineio-*.dist-info/LICENSE*',
     'python-socketio':
-        'venv/lib/python3.7/site-packages/python_socketio-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/python_socketio-*.dist-info/LICENSE*',
     'six':
-        'venv/lib/python3.7/site-packages/six-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/six-*.dist-info/LICENSE*',
     'Werkzeug':
-        'venv/lib/python3.7/site-packages/Werkzeug-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/Werkzeug-*.dist-info/LICENSE*',
     'WTForms':
-        'venv/lib/python3.7/site-packages/WTForms-*.dist-info/LICENSE*',
+        './venv/lib/python3.7/site-packages/WTForms-*.dist-info/LICENSE*',
+
+    # Indirect dependencies through Janus.
+    'libnice':
+        'https://gitlab.freedesktop.org/libnice/libnice/-/blob/0.1.18/COPYING',
+    'librtsp':
+        'https://libwebsockets.org/git/libwebsockets/tree/LICENSE?h=v3.2-stable',
 }
 
-blueprint = flask.Blueprint('py_license', __name__, url_prefix='/py-license')
+blueprint = flask.Blueprint('py_license', __name__, url_prefix='/license')
 
 
 @blueprint.route('/<project>', methods=['GET'])
@@ -56,17 +84,27 @@ def python_project_license_get(project):
     Returns:
         The contents of the project's license file with text/plain content type.
     """
+
     try:
-        return _make_plaintext_response(_get_license_path(project))
+        license_path = _get_license_pattern(project)
     except KeyError:
         return flask.make_response('Unknown project', 404)
 
+    if _is_url(license_path):
+        return _make_redirect_response(license_path)
+    return _make_plaintext_response(license_path)
 
-def _get_license_path(project):
-    return glob.glob(_LICENSE_PATTERNS[project])[0]
+
+def _get_license_pattern(project):
+    return _LICENSE_FILE_GLOB_PATTERNS[project]
 
 
-def _make_plaintext_response(license_path):
+def _is_url(license_pattern):
+    return license_pattern.startswith('http')
+
+
+def _make_plaintext_response(license_pattern):
+    license_path = glob.glob(license_pattern)[0]
     response = flask.make_response(_read_file(license_path), 200)
     response.mimetype = 'text/plain'
     return response
@@ -75,3 +113,7 @@ def _make_plaintext_response(license_path):
 def _read_file(license_path):
     with open(license_path) as license_file:
         return license_file.read()
+
+
+def _make_redirect_response(license_url):
+    return flask.redirect(license_url, code=302)
