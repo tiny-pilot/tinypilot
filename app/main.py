d4de33cd9eb5c2297a0d7949c83f385ab21a0338
Michael Lynch <git@mtlynch.io>
2022-05-18 09:12:59 -0400

    Rename licensing module to license_notice
    I forgot that TinyPilot Pro already has a file called licensing.py that serves a different purpose.

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ from werkzeug import exceptions
 import log
 import api
 import json_response
-import licensing
+import license_notice
 import secret_key
 import socket_api
 import views
@@ -47,7 +47,7 @@ app.config.from_envvar('APP_SETTINGS_FILE')
 csrf = flask_wtf.csrf.CSRFProtect(app)
 
 app.register_blueprint(api.api_blueprint)
-app.register_blueprint(licensing.blueprint)
+app.register_blueprint(license_notice.blueprint)
 app.register_blueprint(views.views_blueprint)
 
 
