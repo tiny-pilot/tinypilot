e33975bcb8c7c7a7b0b472c49bdd8f2e686299b9
ddong4 <ddong4@seas.upenn.edu>
2022-06-18 16:10:50 -0500

    Suppress 'using-with' on manual resource dealloc

--- app/atomic_file_test.py
+++ app/atomic_file_test.py
@@ -14,6 +14,9 @@ class CallerError(Exception):
 class AtomicFileTest(unittest.TestCase):
 
     def setUp(self):
+        # Ignore pylint because we perform a tear down
+        # and assert the temporary files are gone.
+        # pylint: disable=consider-using-with
         self.destination_dir = tempfile.TemporaryDirectory()
 
         self.temp_dir = tempfile.TemporaryDirectory()
