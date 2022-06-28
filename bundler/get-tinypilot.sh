16a6eec199cada2c349c166969101acaa26afad8
Jason Wallace <jdeanwallace@gmail.com>
2022-06-28 14:40:59 +0200

    Implement get-tinypilot script.

--- bundler/get-tinypilot.sh
+++ bundler/get-tinypilot.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# “Mock” version of the eventual `get-tinypilot.sh` script.
-# Instead of downloading the installation bundle on the fly, it expects a
-# tarball named `tinypilot.tgz` to be present in the same directory.
+# Download and install the latest version of TinyPilot Community.
 
 # Exit on first error.
 set -e
@@ -13,19 +11,42 @@ set -u
 # Echo commands to stdout.
 set -x
 
-TEMP_FOLDER="$(mktemp -d)"
-readonly TEMP_FOLDER
+BUNDLE_FILENAME="$(mktemp)"
+readonly BUNDLE_FILENAME
+
+BUNDLE_DIR="$(mktemp -d)"
+readonly BUNDLE_DIR
+
+# Remove temporary files & directories.
+clean_up() {
+  rm -rf "${BUNDLE_FILENAME}" "${BUNDLE_DIR}"
+}
+
+# Download latest tarball to temporary file.
+download() {
+  local HTTP_CODE
+  HTTP_CODE="$(curl https://gk.tinypilotkvm.com/community/download/latest \
+    --location \
+    --output "${BUNDLE_FILENAME}" \
+    --write-out '%{http_code}' \
+    --silent)"
+  if [[ "${HTTP_CODE}" != "200" ]]; then
+    return 1
+  fi
+}
+
+# Always clean up before exiting.
+trap 'clean_up' ERR
+
+# Download tarball to temporary file.
+download
 
 # Extract tarball to temporary folder and run install.
 tar \
   --gunzip \
   --extract \
-  --file tinypilot.tgz \
-  --directory "${TEMP_FOLDER}"
-pushd "${TEMP_FOLDER}"
+  --file "${BUNDLE_FILENAME}" \
+  --directory "${BUNDLE_DIR}"
+pushd "${BUNDLE_DIR}"
 chmod +x install
 ./install
-popd
-
-# Clean up.
-rm -rf "${TEMP_FOLDER}"
