fa2a5fed814e3ae80415b58918d71e28fbcd10c7
Jason Wallace <jdeanwallace@gmail.com>
2022-06-29 14:59:15 +0200

    Implement `get-tinypilot.sh` script. (#1035)
    * Implement get-tinypilot script.
    
    * Fix trap to run on exit.
    
    * Use long mktemp options.
    
    * Add error message if download fails.
    
    * Remove unnecessary chmod.
    
    * Rename folder to directory.
    
    * Inline the downloading of the tarball.
    
    * Move get-tinypilot.sh script to root.

---
+++ get-tinypilot.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Download and install the latest version of TinyPilot Community.
+
+# Exit on first error.
+set -e
+
+# Exit on unset variable.
+set -u
+
+# Echo commands to stdout.
+set -x
+
+BUNDLE_FILENAME="$(mktemp --suffix .tgz)"
+readonly BUNDLE_FILENAME
+
+BUNDLE_DIR="$(mktemp --directory)"
+readonly BUNDLE_DIR
+
+# Remove temporary files & directories.
+clean_up() {
+  rm -rf "${BUNDLE_FILENAME}" "${BUNDLE_DIR}"
+}
+
+# Always clean up before exiting.
+trap 'clean_up' EXIT
+
+# Download tarball to temporary file.
+HTTP_CODE="$(curl https://gk.tinypilotkvm.com/community/download/latest \
+  --location \
+  --output "${BUNDLE_FILENAME}" \
+  --write-out '%{http_code}' \
+  --silent)"
+readonly HTTP_CODE
+if [[ "${HTTP_CODE}" != "200" ]]; then
+  echo "Failed to download tarball with HTTP response status code ${HTTP_CODE}." >&2
+  exit 1
+fi
+
+# Extract tarball to temporary directory and run install.
+tar \
+  --gunzip \
+  --extract \
+  --file "${BUNDLE_FILENAME}" \
+  --directory "${BUNDLE_DIR}"
+pushd "${BUNDLE_DIR}"
+./install
