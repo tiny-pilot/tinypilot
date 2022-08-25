71a2c5ab7a17564ea2ba74dc52af9fa40ba22c1d
Michael Lynch <git@mtlynch.io>
2022-08-25 17:30:23 -0400

    Remove word and reflow

--- app/hid/keyboard.py
+++ app/hid/keyboard.py
@@ -10,10 +10,10 @@ def send_keystroke(keyboard_path, control_keys, hid_keycode):
     # If it's a normal keycode (i.e. not a standalone modifier key), add a
     # message indicating that the key should be released after it is sent. We do
     # this to prevent the keystroke from incorrectly repeating on the target
-    # machine if network latency causes a delay between the keydown event and
-    # keyup events. However, auto-releasing has the disadvantage of preventing
+    # machine if network latency causes a delay between the keydown and keyup
+    # events. However, auto-releasing has the disadvantage of preventing
     # genuinely long key presses (see
-    #  https://github.com/tiny-pilot/tinypilot/issues/1093).
+    # https://github.com/tiny-pilot/tinypilot/issues/1093).
     if hid_keycode:
         release_keys(keyboard_path)
 
