1ac291a7725a6a4ae3996cc2fd66a3ca825d430b
Michael Lynch <git@mtlynch.io>
2022-10-21 08:56:11 -0400

    Fix indentation

--- app/db/migrations/003-wake-on-lan-create.sql
+++ app/db/migrations/003-wake-on-lan-create.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS wake_on_lan(
     id INTEGER PRIMARY KEY,
     mac_address TEXT NOT NULL UNIQUE
-    );
+);
--- app/db/migrations/004-users-add-col-creds-changed.sql
+++ app/db/migrations/004-users-add-col-creds-changed.sql
@@ -2,5 +2,5 @@
 -- value is only needed for previously existing rows.
 
 ALTER TABLE users
-    ADD COLUMN credentials_last_changed
-    TEXT NOT NULL DEFAULT "0001-01-01T00:00:00.000000+00:00";
+ADD COLUMN credentials_last_changed
+TEXT NOT NULL DEFAULT "0001-01-01T00:00:00.000000+00:00";
