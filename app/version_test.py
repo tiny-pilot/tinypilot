cd89df79d5532a64589796494e464bdd8ab24f3b
jotaen4tinypilot <83721279+jotaen4tinypilot@users.noreply.github.com>
2022-12-20 14:59:18 +0100

    Switch to `x.y.z-i+hhhhhhh`-style versioning scheme (#1235)
    Resolves https://github.com/tiny-pilot/tinypilot/issues/1213, part of
    https://github.com/tiny-pilot/gatekeeper/issues/108.
    
    This PR abandons the pure short hash version strings, and compiles
    version numbers in the `x.y.z-i-hhhhhhh` format into the bundle, such as
    `1.8.0-16+649a6b2` (i.e. 16 commits past the `1.8.0` tag, with git
    commit hash `649a6b2`) or `1.8.0-0+0882c51` (i.e. 0 commits past the
    `1.8.0` tag).
    
    These changes correspond to [this Gatekeeper
    PR](https://github.com/tiny-pilot/gatekeeper/pull/110).
    
    The *actual*, functional change is in
    [`build-debian-pkg`](https://github.com/tiny-pilot/tinypilot/blob/semver-versioning/dev-scripts/build-debian-pkg).
    The rest is adjusting commentary and tests.
    
    In the about dialog, I would suggest to [display the version in a
    slightly less technical
    way](https://user-images.githubusercontent.com/83721279/208463277-55c0784d-3e78-4678-a7dc-89ce6c151d56.png).
    We could also consider to strip off the build hash part there, though,
    because we [have in the full in the logs
    dialog](https://user-images.githubusercontent.com/83721279/208463496-cc9325a0-d6b5-4cd5-a5e3-3b982a91992d.png).
    
    <a data-ca-tag
    href="https://codeapprove.com/pr/tiny-pilot/tinypilot/1235"><img
    src="https://codeapprove.com/external/github-tag-allbg.png" alt="Review
    on CodeApprove" /></a>

--- app/version_test.py
+++ app/version_test.py
@@ -21,22 +21,22 @@ class VersionTest(TestCase):
     def test_local_version_when_file_exists(self):
         with tempfile.NamedTemporaryFile('w',
                                          encoding='utf-8') as mock_version_file:
-            mock_version_file.write('1234567')
+            mock_version_file.write('1.2.3-16+7a6c812')
             mock_version_file.flush()
 
             with mock.patch.object(version, '_VERSION_FILE',
                                    mock_version_file.name):
-                self.assertEqual('1234567', version.local_version())
+                self.assertEqual('1.2.3-16+7a6c812', version.local_version())
 
     def test_local_version_strips_leading_trailing_whitespace(self):
         with tempfile.NamedTemporaryFile('w',
                                          encoding='utf-8') as mock_version_file:
-            mock_version_file.write('    1234567   \n')
+            mock_version_file.write('    1.2.3-16+7a6c812   \n')
             mock_version_file.flush()
 
             with mock.patch.object(version, '_VERSION_FILE',
                                    mock_version_file.name):
-                self.assertEqual('1234567', version.local_version())
+                self.assertEqual('1.2.3-16+7a6c812', version.local_version())
 
     def test_local_version_raises_file_error_when_file_doesnt_exist(self):
         with mock.patch.object(version, '_VERSION_FILE',
@@ -67,11 +67,11 @@ class VersionTest(TestCase):
     def test_latest_version_when_request_is_successful(self, mock_urlopen):
         mock_response = mock.Mock()
         mock_response.read.return_value = json.dumps({
-            'version': '1234567'
+            'version': '1.2.3-16+7a6c812'
         }).encode('utf-8')
         mock_urlopen.return_value.__enter__.return_value = mock_response
 
-        self.assertEqual('1234567', version.latest_version())
+        self.assertEqual('1.2.3-16+7a6c812', version.latest_version())
 
     @mock.patch.object(urllib.request, 'urlopen')
     def test_latest_version_raises_request_error_when_response_is_not_utf_8(
@@ -131,4 +131,4 @@ class DebugModeVersionTest(TestCase):
     def test_local_version_returns_dummy_version_when_in_debug_mode(self):
         # Enable debug mode.
         with mock.patch.object(version, '_is_debug', return_value=True):
-            self.assertEqual('0000000', version.local_version())
+            self.assertEqual('0.0.0-0+aaaaaaa', version.local_version())
