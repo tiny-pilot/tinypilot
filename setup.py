eb6452dd1e0771d0c40466819c5eb25ee41fcbbe
Michael Lynch <git@mtlynch.io>
2022-12-28 03:09:49 -0500

    Make setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setup(
     description="Use your Raspberry Pi as a browser-based KVM.",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["eventlet", "Flask", "Flask-SocketIO", "Flask-WTF"],
+    install_requires=[
+        "eventlet==0.31.0", "Flask==1.1.1", "Flask-SocketIO==5.0.1",
+        "Flask-WTF==0.14.3", "pyyaml==5.4.1"
+    ],
     entry_points={"console_scripts": ["tinypilot = app.main:main"]},
 )
