9e5db8177a8d001e8e3bb4584a435b3deb16f702
jotaen4tinypilot <83721279+jotaen4tinypilot@users.noreply.github.com>
2023-04-26 21:38:36 +0200

    Add collapse button to on-screen keyboard (#1375)
    Part of https://github.com/tiny-pilot/tinypilot/issues/1304; stacked on
    https://github.com/tiny-pilot/tinypilot/pull/1374.
    
    This PR adds a collapse button to the on-screen keyboard, so that the
    keyboard can be stashed away more conveniently.
    
    
    https://user-images.githubusercontent.com/83721279/234397126-d60ee922-eeec-42e9-83d4-7b68ad976df6.mov
    
    Notes:
    
    - We don’t (want to) have an equivalent shortcut for showing the
    keyboard for now. This would probably have to live in the status bar,
    and as discussed in [the
    ticket](https://github.com/tiny-pilot/tinypilot/issues/1304), this is a
    bit of a bigger topic in itself.
    - Since the keyboard can be toggled from multiple places now, I had to
    restructure the code around updating the view settings and menu bar
    status. I thought it made most sense to leave the responsibility in
    `app.js`, and couple it loosely via a callback (`onVisibilityChanged`).
    <a data-ca-tag
    href="https://codeapprove.com/pr/tiny-pilot/tinypilot/1375"><img
    src="https://codeapprove.com/external/github-tag-allbg.png" alt="Review
    on CodeApprove" /></a>

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -264,12 +264,6 @@ function setCursor(cursor, save = true) {
   }
 }
 
-function setKeyboardVisibility(isVisible) {
-  document.getElementById("on-screen-keyboard").show(isVisible);
-  settings.setKeyboardVisibility(isVisible);
-  document.getElementById("menu-bar").isKeyboardVisible = isVisible;
-}
-
 function setKeystrokeHistoryStatus(isEnabled) {
   if (isEnabled) {
     settings.enableKeystrokeHistory();
@@ -309,6 +303,14 @@ window.addEventListener("blur", () => {
     );
 });
 
+const onScreenKeyboard = document.getElementById("on-screen-keyboard");
+onScreenKeyboard.addEventListener("keyboard-visibility-changed", (evt) => {
+  const isVisible = evt.detail.isVisible;
+  settings.setKeyboardVisibility(isVisible);
+  document.getElementById("menu-bar").isKeyboardVisible = isVisible;
+});
+onScreenKeyboard.show(settings.isKeyboardVisible());
+
 const menuBar = document.getElementById("menu-bar");
 menuBar.cursor = settings.getScreenCursor();
 menuBar.addEventListener("cursor-selected", (evt) => {
@@ -320,8 +322,7 @@ menuBar.addEventListener("keystroke-history-toggled", () => {
   setKeystrokeHistoryStatus(!isEnabled);
 });
 menuBar.addEventListener("keyboard-visibility-toggled", () => {
-  const onScreenKeyboard = document.getElementById("on-screen-keyboard");
-  setKeyboardVisibility(!onScreenKeyboard.isShown());
+  onScreenKeyboard.show(!onScreenKeyboard.isShown());
 });
 menuBar.addEventListener("shutdown-dialog-requested", () => {
   document.getElementById("shutdown-overlay").show();
@@ -380,7 +381,7 @@ menuBar.addEventListener("ctrl-alt-del-requested", () => {
     code: "Delete",
   });
 });
-setKeyboardVisibility(settings.isKeyboardVisible());
+
 setKeystrokeHistoryStatus(settings.isKeystrokeHistoryEnabled());
 
 document
