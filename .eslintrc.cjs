0b18f79b0c95c6f3fc0a5c8243f15708a8b95dc9
Michael Lynch <git@mtlynch.io>
2023-05-11 17:04:08 -0400

    Revert "work in progress"
    This reverts commit cb5493fe72f3eb4a2f01c43b244e433b8efdf882.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,5 @@ module.exports = {
       { allow: ["error", "debug"] },
     ],
   },
-  //ignorePatterns: ["venv", "app/static/third-party/**/*.js"],
+  ignorePatterns: ["venv", "app/static/third-party/**/*.js"],
 };
