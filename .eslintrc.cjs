d3ab125d5566a22b73fac475f213115335703b6a
Michael Lynch <git@mtlynch.io>
2023-05-16 11:37:07 -0400

    Add more ESLint rules
    We don't currently violate any of these, but they're helpful in keeping our codebase clean.
    
    Rules reference: https://eslint.org/docs/latest/rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,12 +10,22 @@ module.exports = {
   plugins: ["html"],
   extends: ["eslint:recommended"],
   rules: {
+    "block-scoped-var": "error",
+    eqeqeq: "error",
     // This will produce an error for console.log or console.warn in production
     // and a warning in development.
     "no-console": [
       process.env.NODE_ENV === "production" ? "error" : "warn",
       { allow: ["error", "debug"] },
     ],
+    "no-constant-binary-expression": "error",
+    "no-duplicate-imports": "error",
+    "no-lonely-if": "error",
+    "no-unused-private-class-members": "error",
+    "no-useless-return": "error",
+    "no-var": "error",
+    "one-var-declaration-per-line": "error",
+    strict: "error",
   },
   ignorePatterns: ["app/static/third-party", "playwright-report", "venv"],
 };
