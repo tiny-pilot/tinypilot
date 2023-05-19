154f8ddd86e54cf7ac0c53ee8743b4200d3105cc
Michael Lynch <git@mtlynch.io>
2023-05-19 16:39:20 -0400

    Refactor unit tests

--- app/render_template_test.py
+++ app/render_template_test.py
@@ -7,26 +7,26 @@ import render_template
 class RenderTemplateTest(unittest.TestCase):
 
     def test_renders_when_no_overrides_exist(self):
+        mock_defaults = {'tinypilot_external_port': 123}
+        mock_overrides_file = io.StringIO('')
         mock_template_file = io.StringIO(
             'TinyPilot is listening on port {{ tinypilot_external_port }}.')
-
-        mock_overrides_file = io.StringIO('')
 
         self.assertEqual(
             'TinyPilot is listening on port 123.',
-            render_template.render({'tinypilot_external_port': 123},
-                                   mock_overrides_file, mock_template_file))
+            render_template.render(mock_defaults, mock_overrides_file,
+                                   mock_template_file))
 
     def test_overrides_replace_default_settings(self):
-        mock_template_file = io.StringIO(
-            'TinyPilot is listening on port {{ tinypilot_external_port }}.')
-
+        mock_defaults = {'tinypilot_external_port': 123}
         mock_overrides_file = io.StringIO("""
 ---
 tinypilot_external_port: 321
 """)
+        mock_template_file = io.StringIO(
+            'TinyPilot is listening on port {{ tinypilot_external_port }}.')
 
         self.assertEqual(
             'TinyPilot is listening on port 321.',
-            render_template.render({'tinypilot_external_port': 123},
-                                   mock_overrides_file, mock_template_file))
+            render_template.render(mock_defaults, mock_overrides_file,
+                                   mock_template_file))
