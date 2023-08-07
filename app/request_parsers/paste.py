adbdebbe87084a2693cef3b37431938191ca995b
Jason Wallace <jdeanwallace@gmail.com>
2023-08-07 15:53:22 +0200

    Add docstring.

--- app/request_parsers/paste.py
+++ app/request_parsers/paste.py
@@ -2,6 +2,17 @@ from request_parsers import json
 
 
 def parse_text(request):
+    """
+    Parses the text and browser language properties from the request.
+
+    Args:
+        request: Flask request with the following fields in the JSON body:
+            (str) text
+            (str) language
+
+    Returns:
+        A two-tuple consisting of the parsed text and language.
+    """
     # pylint: disable=unbalanced-tuple-unpacking
     (text,
      language) = json.parse_json_body(request,
