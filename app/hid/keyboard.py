64a9941155985844176eed6bf9db351327c5991d
Jason Wallace <jdeanwallace@gmail.com>
2023-08-08 14:54:15 +0200

    Write multiple keystrokes to buffer at once.

--- app/hid/keyboard.py
+++ app/hid/keyboard.py
@@ -1,11 +1,12 @@
 from hid import write as hid_write
 
+RELEASE_KEYS_BUFFER = bytes([0] * 8)
 
-def send_keystroke(keyboard_path, control_keys, hid_keycode):
-    buf = [0] * 8
+
+def keystroke_to_buffer(control_keys, hid_keycode):
+    buf = bytearray(RELEASE_KEYS_BUFFER)
     buf[0] = control_keys
     buf[2] = hid_keycode
-    hid_write.write_to_hid_interface(keyboard_path, buf)
 
     # If it's a normal keycode (i.e. not a standalone modifier key), add a
     # message indicating that the key should be released after it is sent. We do
@@ -15,8 +16,15 @@ def send_keystroke(keyboard_path, control_keys, hid_keycode):
     # genuinely long key presses (see
     # https://github.com/tiny-pilot/tinypilot/issues/1093).
     if hid_keycode:
-        release_keys(keyboard_path)
+        buf += RELEASE_KEYS_BUFFER
+
+    return buf
+
+
+def send_keystroke(keyboard_path, control_keys, hid_keycode):
+    buf = keystroke_to_buffer(control_keys, hid_keycode)
+    hid_write.write_to_hid_interface(keyboard_path, buf)
 
 
 def release_keys(keyboard_path):
-    hid_write.write_to_hid_interface(keyboard_path, [0] * 8)
+    hid_write.write_to_hid_interface(keyboard_path, RELEASE_KEYS_BUFFER)
