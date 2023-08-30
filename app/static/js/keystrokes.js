33d4350d2abffde98fde934229c932bdd9dbb354
Jason Wallace <jdeanwallace@gmail.com>
2023-08-30 14:49:04 +0200

    Add JSDocs.

--- app/static/js/keystrokes.js
+++ app/static/js/keystrokes.js
@@ -1,4 +1,18 @@
+/**
+ * A mechanism to rate-limit the keystrokes messages sent to backend so that
+ * they don't flood the SocketIO server's message queue and starve the server of
+ * PING/PONG messages, needed to keep the client-server connection alive.
+ * 
+ * The current rate-limiting implementation maintains an array of anonymous
+ * functions that get executed in a first in, first out (FIFO) ordering at a
+ * fixed rate of 20 functions per second (i.e., a function gets executed once
+ * every 50ms). Once a function has been executed, it is removed from the array.
+ * Each enqueue function is responsible for it's own success/error handling.
+ */
 export class RateLimitedKeystrokes {
+  /**
+   * @param {Socket} socket - https://socket.io/docs/v4/client-api/#socket 
+   */
   constructor(socket) {
     this._socket = socket;
     this._eventQueue = [];
@@ -27,12 +41,22 @@ export class RateLimitedKeystrokes {
     });
   }
 
-  // Enqueue a keystroke message to be sent to the backend.
+  /**
+   * Enqueue a keystroke message to be sent to the backend.
+   * @param {Object} keystroke - An object as returned by `processKeystroke`
+   * @returns {Promise<Object>}
+   *     Success case:  the Promise resolves with an empty object.
+   *     Error case:    the Promise rejects with an `Error`.
+   */
   sendKeystroke(keystroke) {
     return new Promise((resolve, reject) => {
       this._queueEvent(() => this._handleKeystroke(keystroke, resolve, reject));
     });
   }
+
+  /**
+   * Enqueue a keyRelease message to be sent to the backend.
+   */
   sendKeyRelease() {
     this._queueEvent(() => this._socket.emit("keyRelease"));
   }
