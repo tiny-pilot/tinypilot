2fa2169644374f10502150233fd189d4bd25d298
Jason Wallace <jdeanwallace@gmail.com>
2023-09-06 14:38:27 +0200

    Refactor timeout.

--- app/hid/write.py
+++ app/hid/write.py
@@ -2,6 +2,7 @@ import dataclasses
 import logging
 import multiprocessing
 import typing
+import functools
 
 logger = logging.getLogger(__name__)
 
@@ -62,7 +63,30 @@ class ProcessWithResult(multiprocessing.Process):
         return self.parent_conn.recv() if self.parent_conn.poll() else None
 
 
-def _write_to_hid_interface_immediately(hid_path, buffer):
+def timeout(function):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        # Writes can hang, for example, when TinyPilot is attempting to write to the
+        # mouse interface, but the target system has no GUI. To avoid locking up the
+        # main server process, perform the HID interface I/O in a separate process.
+        write_process = ProcessWithResult(
+            target=function,
+            args=args,
+            kwargs=kwargs,
+            daemon=True)
+        write_process.start()
+        write_process.join(timeout=0.1)
+        if write_process.is_alive():
+            write_process.kill()
+            _wait_for_process_exit(write_process)
+        result = write_process.result()
+        # If the result is None, it means the write failed to complete in time.
+        if result is None or not result.was_successful():
+            raise TimeoutError
+    return wrapper
+
+
+def _write_to_hid_interface(hid_path, buffer):
     # Force timeout.
     for _ in range(pow(10, 100)):
         pass
@@ -74,26 +98,13 @@ def _write_to_hid_interface_immediately(hid_path, buffer):
             'Failed to write to HID interface: %s. Is USB cable connected?',
             hid_path)
 
-
-def write_to_hid_interface(hid_path, buffer):
-    # Writes can hang, for example, when TinyPilot is attempting to write to the
-    # mouse interface, but the target system has no GUI. To avoid locking up the
-    # main server process, perform the HID interface I/O in a separate process.
-    write_process = ProcessWithResult(
-        target=_write_to_hid_interface_immediately,
-        args=(hid_path, buffer),
-        daemon=True)
-    write_process.start()
-    write_process.join(timeout=0.1)
-    if write_process.is_alive():
-        write_process.kill()
-        _wait_for_process_exit(write_process)
-    result = write_process.result()
-    # If the result is None, it means the write failed to complete in time.
-    if result is None or not result.was_successful():
-        raise TimeoutError(f'Failed to write to HID interface: {hid_path}. '
-                         'Is USB cable connected?')
-
+# Workaround to allow decorated functions to be executed in a thread.
+# For example:
+#     @timeout
+#     def write_to_hid_interface(hid_path, buffer):
+#         ...
+# Raises _pickle.PicklingError: Can't pickle
+write_to_hid_interface = timeout(_write_to_hid_interface)
 
 def _wait_for_process_exit(target_process):
     max_attempts = 3
