2d411f3b70abede69041b3241df6189b7c751f45
Jason Wallace <jdeanwallace@gmail.com>
2023-09-19 14:35:53 +0200

    Consume paste API endpoint (#1626)
    Resolves https://github.com/tiny-pilot/tinypilot/issues/1026
    
    This PR consumes the new `/api/paste` endpoint and removes the,
    now-unused, text-to-keystroke frontend logic.
    
    Demo video:
    
    
    https://github.com/tiny-pilot/tinypilot/assets/6730025/2b3cb2c9-964b-4e90-88c9-7ab13775f02b
    
    ### Notes
    1. Pasting unsupported characters (i.e., `“`, `”`, `–` (en-dash), `—`
    (em-dash), etc.) now displays this error dialog:
        
    <img width="870" alt="Screen Shot 2023-09-15 at 11 20 21"
    src="https://github.com/tiny-pilot/tinypilot/assets/6730025/1ebad5a7-fbb6-40f4-a9fe-317a06d458a8">
    
    Previously, these unsupported characters were silently ignored during a
    paste.
    As [discussed
    here](https://github.com/tiny-pilot/tinypilot/pull/1545#issuecomment-1711939987),
    we plan to further refine this behavior in the future.
    
    ### Peer testing
    This PR is stacked onto the following PRs:
    * https://github.com/tiny-pilot/tinypilot/pull/1625
    * https://github.com/tiny-pilot/tinypilot/pull/1624
    
    You can test them all on device via the following command:
    ```bash
    curl \
      --silent \
      --show-error \
      --location \
      https://raw.githubusercontent.com/tiny-pilot/tinypilot/master/scripts/install-bundle | \
      sudo bash -s -- https://output.circle-artifacts.com/output/job/d842ed53-55a6-490d-9cfd-842155c7d6e5/artifacts/0/bundler/dist/tinypilot-community-20230919T1127Z-1.9.1-39+3e308b3.tgz && \
      sudo reboot
    ```
    
    <a data-ca-tag
    href="https://codeapprove.com/pr/tiny-pilot/tinypilot/1626"><img
    src="https://codeapprove.com/external/github-tag-allbg.png" alt="Review
    on CodeApprove" /></a>

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,4 @@
-import {
-  isModifierCode,
-  findKeyCode,
-  keystrokeToCanonicalCode,
-  requiresShiftKey,
-} from "./keycodes.js";
+import { isModifierCode, keystrokeToCanonicalCode } from "./keycodes.js";
 import { KeyboardState } from "./keyboardstate.js";
 import { sendKeystroke } from "./keystrokes.js";
 import * as settings from "./settings.js";
@@ -61,13 +56,6 @@ function recalculateMouseEventThrottle(
 
 function unixTime() {
   return new Date().getTime();
-}
-
-function browserLanguage() {
-  if (navigator.languages) {
-    return navigator.languages[0];
-  }
-  return navigator.language || navigator.userLanguage;
 }
 
 const keystrokeHistory = document.getElementById("status-bar").keystrokeHistory;
@@ -216,45 +204,6 @@ function onKeyUp(evt) {
   }
 }
 
-// Translate a single character into a keystroke and sends it to the backend.
-function processTextCharacter(textCharacter, language) {
-  // Ignore carriage returns.
-  if (textCharacter === "\r") {
-    return;
-  }
-
-  const code = findKeyCode([textCharacter.toLowerCase()], language);
-  let friendlyName = textCharacter;
-  // Give cleaner names to keys so that they render nicely in the history.
-  if (textCharacter === "\n") {
-    friendlyName = "Enter";
-  } else if (textCharacter === "\t") {
-    friendlyName = "Tab";
-  }
-
-  processKeystroke({
-    metaLeft: false,
-    metaRight: false,
-    altLeft: false,
-    altRight: false,
-    shiftLeft: requiresShiftKey(textCharacter),
-    shiftRight: false,
-    ctrlLeft: false,
-    ctrlRight: false,
-    key: friendlyName,
-    code: code,
-  });
-}
-
-// Translate a string of text into individual keystrokes and sends them to the
-// backend.
-function processTextInput(textInput) {
-  const language = browserLanguage();
-  for (const textCharacter of textInput) {
-    processTextCharacter(textCharacter, language);
-  }
-}
-
 function setCursor(cursor, save = true) {
   // Ensure the correct cursor option displays as active in the navbar.
   if (save) {
@@ -287,9 +236,6 @@ document.addEventListener("overlay-toggled", (evt) => {
 document.addEventListener("video-streaming-mode-changed", (evt) => {
   document.getElementById("status-bar").videoStreamIndicator.mode =
     evt.detail.mode;
-});
-document.addEventListener("paste-text", (evt) => {
-  processTextInput(evt.detail);
 });
 
 // To allow for keycode combinations to be pressed (e.g., Alt + Tab), the
