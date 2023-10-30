9bdfa236943e90b46518ed968ccbcba4385a6d87
Jason Wallace <jdeanwallace@gmail.com>
2023-10-30 15:31:52 +0200

    Add streaming-mode cli.

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ from werkzeug import exceptions
 # app-wide logger class before any other module loads it.
 import log
 import api
+import cli
 import json_response
 import license_notice
 import secret_key
@@ -49,6 +50,7 @@ csrf = flask_wtf.csrf.CSRFProtect(app)
 app.register_blueprint(api.api_blueprint)
 app.register_blueprint(license_notice.blueprint)
 app.register_blueprint(views.views_blueprint)
+app.register_blueprint(cli.cli_blueprint)
 
 
 @app.errorhandler(flask_wtf.csrf.CSRFError)
