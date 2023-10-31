a8428c695dff04f81e887b978367294006411809
jotaen4tinypilot <83721279+jotaen4tinypilot@users.noreply.github.com>
2023-10-31 14:48:17 +0100

    Support for specifying STUN server: configure client (2.5/3) (#1657)
    Related https://github.com/tiny-pilot/tinypilot/issues/1460.
    
    Now that the [backend is able to re-write the Janus configuration to
    make Janus connect to a STUN
    server](https://github.com/tiny-pilot/tinypilot/pull/1646), this PR adds
    the client counterpart. That means when the user enabled STUN, the
    frontend’s WebRTC library will also be configured to use the same STUN
    server.
    
    This PR complements https://github.com/tiny-pilot/tinypilot/pull/1647,
    and strictly speaking, it even should have preceded it.
    
    Note that the PR review is only about the code itself, but it does
    **not** include an end-to-end test of the full STUN functionality on
    device. Charles thankfully [did that
    separately](https://github.com/tiny-pilot/tinypilot/issues/1460#issuecomment-1765118905).
    (To sum that up: the testing procedure turned out to be rather tricky,
    unfortunately… We still aren’t 100% confident that our STUN
    implementation reliably solves connection issues in remote access
    scenarios, but given the testing complexity, [we decided that the status
    quo has yielded enough evidence for us to move
    forward](https://github.com/tiny-pilot/tinypilot/issues/1460#issuecomment-1781773569).)
    
    Some notes on the code:
    
    - Since the STUN server address comes from the backend and are hence
    validated, I used a rather pragmatic approach in `createIceServerUrls`
    to account for IPv6 addresses.
    - `webrtc-video.js` is of type “module”, so it’s a bit tricky to pass
    parameters to it from the parent script. I think using global variables
    is not super beautiful, but it gets the job done. I’m open for better
    ideas, but the current mechanism would also be “good enough” for me.
    - There is, unfortunately, one quirk with ESLint and Jinja templates,
    regarding the `TINYPILOT_JANUS_STUN_PORT` assignment, where we have to
    apply a rather ugly workaround. As far as I can see, there is not much
    we can do about that.

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ import flask
 
 import db.settings
 import hostname
+import update.settings
 from find_files import find as find_files
 
 views_blueprint = flask.Blueprint('views', __name__, url_prefix='')
@@ -15,9 +16,16 @@ def index_get():
     use_webrtc = db.settings.Settings().get_streaming_mode(
     ) == db.settings.StreamingMode.H264
 
+    try:
+        update_settings = update.settings.load()
+    except update.settings.LoadSettingsError:
+        return flask.abort(500)
+
     return flask.render_template(
         'index.html',
         use_webrtc_remote_screen=use_webrtc,
+        janus_stun_server=update_settings.janus_stun_server,
+        janus_stun_port=update_settings.janus_stun_port,
         page_title_prefix=_page_title_prefix(),
         is_standalone_mode=_is_standalone_mode(),
         custom_elements_files=find_files.custom_elements_files())
