26e1e1de131cad6fb32bdf5fbd4155e74a2c00d8
Jason Wallace <jdeanwallace@gmail.com>
2023-11-02 14:41:09 +0200

    Add video streaming mode to TinyPilot logs (#1666)
    Resolves https://github.com/tiny-pilot/tinypilot/issues/1490
    
    Video demo:
    
    
    https://github.com/tiny-pilot/tinypilot/assets/6730025/0165c85d-f9fa-4389-b98d-44de90843f5a
    
    ### Notes
    
    1. When there are active WebRTC consumers, I see this in the Janus logs:
        ```
    Oct 30 15:27:57 tinypilot janus[1790]: Creating new session:
    3545622190134115; 0xed914e28
    Oct 30 15:27:57 tinypilot janus[1790]: Creating new handle in session
    3545622190134115: 8464226001965214; 0xed914e28 0xefb019c8
    Oct 30 15:27:57 tinypilot janus[1790]: == ustreamer/main -- Creating
    session 0xefb01650 ...
    Oct 30 15:27:57 tinypilot janus[1790]: == ustreamer/video -- Memsink
    opened; reading frames ...
    Oct 30 15:27:57 tinypilot janus[1790]: [8464226001965214] Creating ICE
    agent (ICE Full mode, controlling)
    Oct 30 15:27:57 tinypilot janus[1790]: == ustreamer/audio -- Detected
    host audio
    Oct 30 15:27:57 tinypilot janus[1790]: == ustreamer/audio -- Pipeline
    configured on 48000Hz; capturing ...
    Oct 30 15:27:58 tinypilot janus[1790]: [8464226001965214] The DTLS
    handshake has been completed
        ```
        
    When there are no active WebRTC consumers, I see this in the Janus logs:
        ```
    Oct 30 15:30:15 tinypilot janus[1790]: [WSS-0xed9005f8] Destroying
    WebSocket client
    Oct 30 15:30:15 tinypilot janus[1790]: Destroying session
    3545622190134115; 0xed914e28
    Oct 30 15:30:15 tinypilot janus[1790]: Detaching handle from ustreamer;
    0xefb019c8 0xefb01650 0xefb019c8 (nil)
    Oct 30 15:30:15 tinypilot janus[1790]: [8464226001965214] WebRTC
    resources freed; 0xefb019c8 0xed914e28
    Oct 30 15:30:15 tinypilot janus[1790]: == ustreamer/main -- Removing
    session 0xefb01650 ...
    Oct 30 15:30:15 tinypilot janus[1790]: == ustreamer/audio -- Pipeline
    closed
    Oct 30 15:30:15 tinypilot janus[1790]: == ustreamer/video -- Memsink
    closed
    Oct 30 15:30:16 tinypilot janus[1790]: == ustreamer/video -- No active
    watchers, memsink disconnected
        ```
        
    So based on the above log examples, [H264 mode is
    active](https://github.com/tiny-pilot/tinypilot/blob/dda4d8b161eaac91db7f05a4e6bc14510b99f415/debian-pkg/opt/tinypilot-privileged/scripts/collect-debug-logs#L171-L173)
    when the last Janus log that contains `ustreamer/video`, also contains
    `Memsink opened; reading frames`. For example:
    
        ```
    Oct 30 15:27:57 tinypilot janus[1790]: == ustreamer/video -- Memsink
    opened; reading frames ...
        ```
        
        Otherwise, it is assumed that MJPEG is active.
    
    <a data-ca-tag
    href="https://codeapprove.com/pr/tiny-pilot/tinypilot/1666"><img
    src="https://codeapprove.com/external/github-tag-allbg.png" alt="Review
    on CodeApprove" /></a>

---
+++ app/cli.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+
+import db.settings
+
+# For invoking the commands on the CLI you need to specify the FLASK_APP env
+# variable containing the path to the main file. E.g.: FLASK_APP=app/main.py
+cli_blueprint = Blueprint('cli', __name__)
+
+
+@cli_blueprint.cli.command('streaming-mode')
+def streaming_mode():
+    """Prints TinyPilot's preferred video streaming mode, either H264 or MJPEG.
+
+    Note: this doesn't represent the currently active video streaming mode
+    because H264 can fail and fall back to MJPEG.
+
+    Example of invocation:
+        flask cli streaming-mode
+    """
+    print(db.settings.Settings().get_streaming_mode().value)
