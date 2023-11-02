d01bafe11a7b2c711cae26a8e5d2ba7c28052f3d
Jason Wallace <jdeanwallace@gmail.com>
2023-11-02 14:57:47 +0200

    Add "Pro" stub for Static IP feature.

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -329,6 +329,9 @@ menuBar.addEventListener("mass-storage-dialog-requested", () => {
 menuBar.addEventListener("wake-on-lan-dialog-requested", () => {
   document.getElementById("feature-pro-overlay").show();
 });
+menuBar.addEventListener("static-ip-dialog-requested", () => {
+  document.getElementById("feature-pro-overlay").show();
+});
 menuBar.addEventListener("video-settings-dialog-requested", () => {
   document.getElementById("video-settings-dialog").initialize();
   document.getElementById("video-settings-overlay").show();
