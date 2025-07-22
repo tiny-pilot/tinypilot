29da3e749facfea6fa19e80cf112505c610d4495
db39 <88856877+db39@users.noreply.github.com>
2025-07-22 12:14:50 +0100

    Update license paths (#1898)
    Resolves #1897 
    
    This change updates the paths to the PyYaml and MarkupSafe Python
    dependency licenses.
    
    <a data-ca-tag
    href="https://codeapprove.com/pr/tiny-pilot/tinypilot/1898"><img
    src="https://codeapprove.com/external/github-tag-allbg.png" alt="Review
    on CodeApprove" /></a>
    
    ---------
    
    Co-authored-by: Jason Wallace <jdeanwallace@gmail.com>

--- app/license_notice.py
+++ app/license_notice.py
@@ -94,8 +94,11 @@ _LICENSE_METADATA = [
     LicenseMetadata(
         name='pyyaml',
         homepage_url='https://pyyaml.org',
-        license_glob_pattern=
-        './venv/lib/python3.*/site-packages/PyYAML-*.dist-info/LICENSE*',
+        # We're unable to consistently link to the local license file because of
+        # inconsistent Python package structure.
+        # https://github.com/tiny-pilot/tinypilot/issues/1899
+        license_url=
+        'https://raw.githubusercontent.com/yaml/pyyaml/6.0.2/LICENSE',
     ),
     LicenseMetadata(
         name='bidict',
@@ -154,8 +157,11 @@ _LICENSE_METADATA = [
     LicenseMetadata(
         name='MarkupSafe',
         homepage_url='https://palletsprojects.com/p/markupsafe/',
-        license_glob_pattern=
-        './venv/lib/python3.*/site-packages/MarkupSafe-*.dist-info/LICENSE*',
+        # We're unable to consistently link to the local license file because of
+        # inconsistent Python package structure.
+        # https://github.com/tiny-pilot/tinypilot/issues/1899
+        license_url=
+        'https://raw.githubusercontent.com/pallets/markupsafe/3.0.2/LICENSE.txt',
     ),
     LicenseMetadata(
         name='python-dotenv',
